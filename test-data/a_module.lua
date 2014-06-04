887b63b7fcbec0b5b6c72084a0bb1df371c779d7
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2014-06-04 10:00:06 +0200

    Trying to test the HTTP require.

--- test-data/a_module.lua
+++ test-data/a_module.lua
@@ -1,23 +1,4 @@
--- Tests for the extension of `require` to HTTP URLs
--- =================================================
-
--- These tests depend on `luassert` for assertions.
-local assert = require "luassert"
-
--- Without the extension, a URL cannot be loaded as a Lua module.
-assert.has.error (function ()
-  require "https://raw.githubusercontent.com/CosyVerif/lang/master/src/cosy/util/http_require_data.lua"
-end)
-
--- With the extension, a URL can be loaded.
-assert.has.no.error (function ()
-  require "cosy.util.http_require"
-  require "https://raw.githubusercontent.com/CosyVerif/lang/master/src/cosy/util/http_require_data.test.lua"
-end)
-
--- The extended `require` loads effectively the required module.
-local remote_module = require "https://raw.githubusercontent.com/CosyVerif/lang/master/src/cosy/util/http_require_data.test.lua"
-local local_module = require "cosy.util.http_require_data.test"
-
--- The distant module and its local equivalent are the same:
-assert.are.same (local_module, distant_module)
+return {
+  a = true,
+  b = true,
+}
