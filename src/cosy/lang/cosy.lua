0e5d28ab8e799c4e67580bb73e8f91bca4907493
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2014-07-09 14:29:06 +0200

    Cosy is global and fixed.

--- src/cosy/lang/cosy.lua
+++ src/cosy/lang/cosy.lua
@@ -1,5 +1,12 @@
 local global = _ENV or _G
 
 global.cosy = {}
+global.cosy.tags = require "cosy.lang.tags"
+
+local cosy = global.cosy
+local NAME = cosy.tags.NAME
+
+cosy [NAME] = "cosy"
+NAME.persistent = true
 
 return cosy
