028f432b712ef41bf5716f33fabe0b1f30f1c154
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2014-07-18 23:29:22 +0200

    First version of path proxy.

---
+++ src/cosy/proxy/path.lua
@@ -0,0 +1,36 @@
+local raw   = require "cosy.util.raw"
+local proxy = require "cosy.util.proxy"
+local copy  = require "cosy.util.shallow_copy"
+local tags  = require "cosy.util.tags"
+
+local DATA  = tags.DATA
+local PATH  = tags.PATH
+
+local path = proxy {}
+
+function path:__index (key)
+  if key == PATH then
+    rawset (self, PATH, { raw (self) })
+    return self [PATH]
+  end
+  local below  = self [DATA]
+  local result = self (below)
+  local p = copy (self [PATH])
+  p [#p + 1] = key
+  rawset (result, PATH, p)
+  return result
+end
+
+function path:__newindex (key, value)
+  if key == PATH then
+    error "Trying to set the PATH attribute."
+  end
+  local p = copy (self [PATH])
+  p [#p + 1] = key
+  local v = self (value)
+  rawset (v, PATH, p)
+  local below = self [DATA]
+  below [key] = value
+end
+
+return path
---
+++ src/cosy/proxy/path.test.lua
@@ -0,0 +1,62 @@
+local assert = require "luassert"
+local path   = require "cosy.proxy.path"
+local tags   = require "cosy.util.tags"
+local PATH   = tags.PATH
+
+do
+  local data = {}
+  local p = path (data)
+  assert.are.same (p [PATH], { data })
+end
+
+do
+  local data = {}
+  local p = path (data)
+  local q = p.some_key
+  assert.are.same (p [PATH], { data })
+  assert.are.same (q [PATH], { data, "some_key" })
+end
+
+do
+  local data = {}
+  local p = path (data)
+  local NIL = {}
+  for _, value in ipairs {
+    NIL,
+    true,
+    0,
+    "",
+    { "" },
+    function () end,
+    coroutine.create (function () end),
+  } do
+    if value == NIL then
+      value = nil
+    end
+    p.some_key = value
+    assert.are.same (p [PATH], { data })
+    assert.are.same (p.some_key [PATH], { data, "some_key" })
+  end
+end
+
+do
+  local data = {}
+  local p = path (data)
+  local NIL = {}
+  for _, value in ipairs {
+    NIL,
+    true,
+    0,
+    "",
+    { "" },
+    function () end,
+    coroutine.create (function () end),
+  } do
+    if value == NIL then
+      value = nil
+    end
+    local q = p (value)
+    assert.are.same (p [PATH], { data })
+    assert.are.same (q [PATH], { value })
+  end
+end
