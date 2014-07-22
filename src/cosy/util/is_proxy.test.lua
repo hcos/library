c9461feeb457368ec247c69bcf5d5469e2fd23ff
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2014-07-22 22:17:41 +0200

    Update tests for is_proxy.

--- src/cosy/util/is_proxy.test.lua
+++ src/cosy/util/is_proxy.test.lua
@@ -3,16 +3,13 @@ local is_proxy = require "cosy.util.is_proxy"
 local proxy    = require "cosy.util.proxy"
 
 do
-
   local a_proxy = proxy ()
-
   local NIL = {}
   for _, value in ipairs {
     NIL,
     true,
     0,
     "",
-    { "" },
     function () end,
     coroutine.create (function () end),
   } do
@@ -20,7 +17,16 @@ do
       value = nil
     end
     assert.is_falsy (is_proxy (value))
+    assert.is_falsy (is_proxy (a_proxy (value)))
+  end
+end
+
+do
+  local a_proxy = proxy ()
+  for _, value in ipairs {
+    { "" },
+  } do
+    assert.is_falsy (is_proxy (value))
     assert.is_true  (is_proxy (a_proxy (value)))
   end
-
 end
