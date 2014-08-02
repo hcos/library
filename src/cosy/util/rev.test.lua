b66d4b40b6f5a42419d7b8c09a7bbccf1f7627f7
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2014-08-02 15:57:41 +0200

    Add reverse sequence iterator.

---
+++ src/cosy/util/rev.test.lua
@@ -0,0 +1,57 @@
+-- Iterators on Sequences
+-- ======================
+--
+-- All iterators should return nil when applied on a non table, as it
+-- prevent iterating of these data.
+--
+local assert = require "luassert"
+local rev    = require "cosy.util.rev"
+
+do
+  assert.is_true (rev (nil) () == nil)
+  for _, x in ipairs {
+    true,
+    0,
+    "",
+    function () end,
+    coroutine.create (function () end),
+  } do
+    assert.is_true (rev (x) () == nil)
+  end
+end
+
+-- Behavior on tables
+-- ------------------
+--
+-- In order to test the iterators, we build the following data. It has some
+-- interesting characteristics:
+--
+-- * some keys are associated with the `false` value, and thus must not be
+--   taken into account for the `set` iterator;
+-- * some keys are numeric, starting from `1`, are thus used by `rev`,
+-- * a numeric key is not following its `n-1` key, and thus is not taken
+--   into account by `rev`.
+local data = {
+  [1] = "one",
+  [2] = "two",
+  [3] = false,
+  [5] = true,
+  a   = "three",
+  b   = "four",
+  z   = false,
+}
+
+-- ### `rev` Iterator
+--
+-- The `rev` iterator lists all the values that are obtained by iterating
+-- sequentially over keys starting from the last element.
+--
+do
+  local values = {}
+  for v in rev (data) do
+    values [#values + 1] = v
+  end
+  assert.are.same (values, {
+    false, "two", "one",
+  })
+end
