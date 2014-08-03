501cabb2e8ae61f852ded99e9438147ff259be45
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2014-08-03 13:34:10 +0200

    More on the protocol, but not finished.

--- src/cosy/protocol.lua
+++ src/cosy/protocol.lua
@@ -1,16 +1,7 @@
-local sha1   = require "sha1"
-local json   = require "dkjson"
 local rev    = require "cosy.util.rev"
 local seq    = require "cosy.util.seq"
-local set    = require "cosy.util.set"
-local map    = require "cosy.util.map"
-local tags   = require "cosy.util.tags"
-local raw    = require "cosy.util.raw"
-local ignore = require "cosy.util.ignore"
 
-local rawify        = require "cosy.proxy.rawify"
-local remember_path = require "cosy.proxy.remember_path"
-local guess_patch   = require "cosy.proxy.guess_patch"
+local cosy, tags = require "cosy.def"
 
 local IS_VOLATILE = tags.IS_VOLATILE
 local PATCHES     = tags.PATCHES
@@ -18,7 +9,6 @@ local RESOURCE    = tags.RESOURCE
 local TOKEN       = tags.TOKEN
 local INTERFACE   = tags.INTERFACE
 local VERSION     = tags.VERSION
-local REQUESTS    = tags.REQUESTS
 
 PATCHES   [IS_VOLATILE] = true
 RESOURCE  [IS_VOLATILE] = true
@@ -26,44 +16,32 @@ TOKEN     [IS_VOLATILE] = true
 INTERFACE [IS_VOLATILE] = true
 VERSION   [IS_VOLATILE] = true
 
-local from_user_wrapper =
-  guess_patch .. remember_path .. rawify
-local unapply_wrapper =
-  rawify
-local from_server_wrapper =
-  rawify
-
-local cosy = {}
-local global = _G or _ENV
-global.tags  = tags
-global.cosy  = wrapper (cosy)
-
-local protocol   = {}
+local protocol = {}
 
 -- Called by the interface:
 function protocol.on_connect (parameters)
-  local resource = parameters.resource
-  local token    = parameters.token
-  local ws       = parameters.ws
-  ws:log ("Connecting to resource: " .. tostring (resource) ..
-          " using token: " .. tostring (token))
-  cosy [resource] = {
+  local resource  = parameters.resource
+  local token     = parameters.token
+  local interface = parameters.interface
+  interface:log ("Connecting to resource: " .. tostring (resource) ..
+                 " using token: " .. tostring (token))
+  cosy [resource] = { -- TODO: wrap resource
     [PATCHES  ] = {},
     [RESOURCE ] = resource,
     [TOKEN    ] = token,
-    [INTERFACE] = ws,
+    [INTERFACE] = interface,
     [VERSION  ] = nil,
   }
 end
 
 function protocol.on_open (model)
-  local resoure = model [RESOURCE]
-  local token   = model [TOKEN]
-  local ws      = model [INTERFACE]
-  ws:log ("Sending set-resource command" ..
-          " for: " .. tostring (resource) ..
-          " using token: " .. tostring (token))
-  ws:send {
+  local resource  = model [RESOURCE]
+  local token     = model [TOKEN]
+  local interface = model [INTERFACE]
+  interface:log ("Sending set-resource command" ..
+                 " for: " .. tostring (resource) ..
+                 " using token: " .. tostring (token))
+  interface:send {
     action   = "set-resource",
     token    = token,
     resource = resource,
@@ -71,8 +49,9 @@ function protocol.on_open (model)
 end
 
 function protocol.on_close (model)
-  local resource = model [RESOURCE]
-  ws:log ("Closing connection to resource " .. tostring (resource))
+  local resource  = model [RESOURCE]
+  local interface = model [INTERFACE]
+  interface:log ("Closing connection to resource " .. tostring (resource))
   cosy [resource] = nil
 end
 
@@ -102,13 +81,13 @@ end
 -- 
 
 function protocol.on_patch (model)
-  local token   = model [TOKEN]
-  local ws      = model [INTERFACE]
-  local patches = model [PATCHES]
+  local token     = model [TOKEN]
+  local interface = model [INTERFACE]
+  local patches   = model [PATCHES]
   for patch in seq (patches) do
     if patch.status == "applied" then
       local id = tostring (patch)
-      ws:send {
+      interface:send {
         action     = "add-patch",
         token      = token,
         data       = patch.code,
@@ -121,99 +100,123 @@ function protocol.on_patch (model)
 end
 
 function protocol.on_message (model, message)
-  local resoure = model [RESOURCE]
-  local token   = model [TOKEN]
-  local ws      = model [INTERFACE]
-  -- Case 1: answer to get-model
-  -- Case 2: answer to add-patch
-  -- Case 3: update
+  local resource  = model [RESOURCE]
+  local token     = model [TOKEN]
+  local interface = model [INTERFACE]
+  local patches   = model [PATCHES]
   if message.action == "set-resource" then
     if message.accepted then
-      ws:log ("Connected to resource: " .. tostring (resource))
-      ws:log ("Sending get-model command" ..
-              " for resource: " .. tostring (resource) ..
-              " using token: " .. tostring (token))
-      ws:send {
-        action     = "get-model",
-        token      = token,
+      interface:log ("Connected to resource: " .. tostring (resource))
+      interface:log ("Sending get-model command" ..
+                     " for resource: " .. tostring (resource) ..
+                     " using token: " .. tostring (token))
+      interface:send {
+        action = "get-model",
+        token  = token,
       }
     else
-      ws:err ("Unable to connect to resource: " .. tostring (message.reason))
-      ws:close ()
+      interface:err ("Unable to connect to resource: " ..
+                     tostring (message.reason))
+      interface:close ()
     end
   elseif message.action == "get-model" then
     if message.accepted then
-      ws:log ("Loading resource: " .. tostring (resource))
+      interface:log ("Loading resource: " .. tostring (resource))
       local ok, err = pcall (loadstring (message.code))
       if ok then
-        ws:log ("Updating resource: " .. tostring (resource) ..
-                " version to: " .. tostring (message.version))
+        interface:log ("Updating resource: " .. tostring (resource) ..
+                       " version to: " .. tostring (message.version))
         model [VERSION] = message.version
-        ws:ready ()
+        interface:ready ()
       else
-        ws:err ("Unable to load resource: " .. tostring (resource) ..
-                " because: " .. tostring (err))
-        ws:close ()
+        interface:err ("Unable to load resource: " .. tostring (resource) ..
+                       " because: " .. tostring (err))
+        interface:close ()
       end
     else
-      ws:close ()
-      error ("Unable to get resource: " .. tostring (resource) .. 
-             " because: " .. tostring (message.reason))
+      interface:err ("Unable to get resource: " .. tostring (resource) .. 
+                     " because: " .. tostring (message.reason))
+      interface:close ()
     end
   elseif message.action == "add-patch" then
+    local patch = patches [1]
+    assert (message.answer == patch.request_id)
     if model [VERSION] then
       if message.accepted then
-        assert (message.answer == patches [1] . request_id)
-        ws:log ("Patch: " .. tostring (message.id) ..
-                " on resource: " .. tostring (resource) ..
-                " has been accepted")
+        interface:log ("Patch: " .. tostring (message.id) ..
+                       " on resource: " .. tostring (resource) ..
+                       " has been accepted")
+        if patch.status == "unapplied" then
+          interface:log ("Applying unapplied patch: " ..
+                         tostring (patch.id) ..
+                         " on resource: " .. tostring (resource))
+          local ok, err = pcall (loadstring (patch.code))
+          if not ok then
+            interface:err ("Unable to reapply patch: " ..
+                           tostring (patch.id) ..
+                           " on resource: " .. tostring (resource) ..
+                           " because: " .. tostring (err))
+          end
+        end
         model [VERSION] = message.version
         table.remove (patches, 1)
       else
+        interface:log ("Patch: " .. tostring (message.id) ..
+                       " on resource: " .. tostring (resource) ..
+                       " rejected because: " .. tostring (message.reason))
         -- TODO: set correct cosy wrapper
         for patch in rev (patches) do
-          patch.unapply ()
+          if patch.status == "applied" or patch.status == "sent" then
+            patch.unapply ()
+            patch.status = "unapplied"
+          end
         end
-        -- TODO: set correct cosy wrapper
         table.remove (patches, 1)
-        for patch in seq (patches) do
-          local ok, err = pcall (loadstring (patch.code))
-        end
         -- TODO: set correct cosy wrapper
       end
     else
-      ws:err ("Edition of resource: " .. tostring (resource) ..
-              " that has still not been loaded")
+      interface:err ("Edition of resource: " .. tostring (resource) ..
+                     " that has still not been loaded")
     end
   elseif message.action == "update" then
     if model [VERSION] then
       -- TODO: set correct cosy wrapper
+      interface:log ("Unapply patches on resource: " ..
+                     tostring (resource) ..
+                     " because of interleaved update: " ..
+                     tostring (message.id))
       for patch in rev (patches) do
-        patch.unapply ()
+        if patch.status == "applied" or patch.status == "sent" then
+          patch.unapply ()
+          patch.status = "unapplied"
+        end
       end
       -- TODO: set correct cosy wrapper
       for patch in seq (message.patches) do
         if patch.id > model [VERSION] then
+          interface:log ("Applying patch: " .. tostring (patch.id) ..
+                         " on resource: " .. tostring (resource))
           local ok, err = pcall (loadstring (patch.code))
+          if not ok then
+            interface:err ("Unable to apply patch: " ..
+                           tostring (patch.id) ..
+                           " because: " .. tostring (err))
+          end
         else
-          ws:log ("Patch: " .. tostring (patch.id) ..
-                  " has already been applied on resource: " ..
-                  tostring (resource)) 
+          interface:log ("Patch: " .. tostring (patch.id) ..
+                         " already applied on resource: " ..
+                         tostring (resource)) 
         end
       end
       -- TODO: set correct cosy wrapper
-      for patch in seq (patches) do
-        local ok, err = pcall (loadstring (patch.code))
-      end
-      -- TODO: set correct cosy wrapper
     else
-      ws:log ("Receiving patches for resource: " .. tostring (resource) ..
-              " that has still not been loaded")
+      interface:log ("Receiving patches for resource: " ..
+                     tostring (resource) ..
+                     " that has still not been loaded")
     end
   else
-
+    interface:err ("Unexpected message: " .. tostring (message.action))
   end
-  global.cosy = from_user_wrapper (cosy)
 end
 
 return protocol
