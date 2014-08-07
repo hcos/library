59475095bd0a9bb323d47f1129bb5783b4b30ccc
Francisco Gimenez <dc.francisco@gmail.com>
2014-08-07 14:01:19 +0200

    fix issue with arc markers

--- editor.js
+++ editor.js
@@ -138,14 +138,14 @@
         //~ .linkDistance(100)
         .on("tick", tick);
     
-    // Per-type markers, as they don't inherit styles.
     svg.append("svg:defs").selectAll("marker")
-        .data(["suit", "licensing", "resolved"])
-        .enter().append("svg:marker")
+        .data(["licensing"])      // Different link/path types can be defined here
+        .enter().append("svg:marker")    // This section adds in the arrows
         .attr("id", String)
         .attr("viewBox", "0 -5 10 10")
-        .attr("refX", markerWidth +2)
-        .attr("markerWidth", markerWidth)
+        .attr("refX", 8)
+        .attr("markerWidth", 8)
+        .attr("markerHeight", 8)
         .attr("orient", "auto")
         .append("svg:path")
         .attr("d", "M0,-5L10,0L0,5");
