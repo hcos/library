7822ffcc39b2d2d5c64ec7bd235db0bd34a48f6c
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2014-09-18 10:17:40 +0200

    Add reversed and sorted iterators.

--- src/cosy/algorithm.lua
+++ src/cosy/algorithm.lua
@@ -1,9 +1,7 @@
 local Algorithm = {}
 
 function Algorithm.map (data)
-  if type (data) ~= "table" then
-    return function () end
-  end
+  assert (type (data) == "table")
   return coroutine.wrap (
     function ()
       for k, v in pairs (data) do
@@ -14,24 +12,19 @@ function Algorithm.map (data)
 end
 
 function Algorithm.seq (data)
-  if type (data) ~= "table" then
-    return function () end
-  end
-  local f = coroutine.wrap (
+  assert (type (data) == "table")
+  return coroutine.wrap (
     function ()
-      for _, v in ipairs (data) do
-        coroutine.yield (v)
+      for i, v in ipairs (data) do
+        coroutine.yield (i, v)
       end
     end
   )
-  return f
 end
 
 function Algorithm.set (data)
-  if type (data) ~= "table" then
-    return function () end
-  end
-  local f = coroutine.wrap (
+  assert (type (data) == "table")
+  return coroutine.wrap (
     function ()
       for k, v in pairs (data) do
         if v then
@@ -40,7 +33,51 @@ function Algorithm.set (data)
       end
     end
   )
-  return f
+end
+
+function Algorithm.reversed (iterator)
+  local results = {}
+  for k, v in iterator do
+    results [#results + 1] = {
+      k = k,
+      v = v,
+    }
+  end
+  return coroutine.wrap (
+    function ()
+      for i = #results, 1, -1 do
+        local result = results [i]
+        coroutine.yield (result.k, result.v)
+      end
+    end
+  )
+end
+
+local function sort (lhs, rhs)
+  if type (lhs) < type (rhs) then
+    return true
+  elseif type (lhs) > type (rhs) then
+    return false
+  else
+    return lhs < rhs
+  end
+end
+
+function Algorithm.sorted (iterator)
+  local keys    = {}
+  local results = {}
+  for k, v in iterator do
+    keys [#keys + 1] = k
+    results [k] = v
+  end
+  table.sort (keys, sort)
+  return coroutine.wrap (
+    function ()
+      for _, k in ipairs (keys) do
+        coroutine.yield (k, results [k])
+      end
+    end
+  )
 end
 
 return Algorithm
--- src/cosy/algorithm.test.lua
+++ src/cosy/algorithm.test.lua
@@ -49,7 +49,7 @@ do
     z   = false,
   }
   local values = {}
-  for v in Algorithm.seq (data) do
+  for _, v in Algorithm.seq (data) do
     values [#values + 1] = v
   end
   assert.are.same (values, {
@@ -80,3 +80,49 @@ do
     ["b"] = true,
   })
 end
+
+-- reversed
+do
+  local data = {
+    [1] = "one",
+    [2] = "two",
+    [3] = false,
+    [5] = true,
+    a   = "three",
+    b   = "four",
+    z   = false,
+  }
+  local values = {}
+  for _, v in Algorithm.reversed (Algorithm.seq (data)) do
+    values [#values + 1] = v
+  end
+  assert.are.same (values, {
+    false, "two", "one",
+  })
+end
+
+-- sorted
+do
+  local data = {
+    [1] = "one",
+    [2] = "two",
+    [3] = false,
+    [5] = true,
+    a   = "three",
+    b   = "four",
+    z   = false,
+  }
+  local keys   = {}
+  local values = {}
+  for k, v in Algorithm.sorted (Algorithm.map (data)) do
+    keys   [#keys   + 1] = k
+    values [#values + 1] = v
+  end
+  assert.are.same (keys, {
+    1, 2, 3, 5, "a", "b", "z",
+  })
+  assert.are.same (values, {
+    "one", "two", false, true, "three", "four", false,
+  })
+end
+
