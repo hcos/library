ffa0cb65ef46572786a07e4eaf74b9bc3e4b0407
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2014-09-18 14:15:41 +0200

    Start cosy main library.

--- src/cosy.lua
+++ src/cosy.lua
@@ -1,1 +1,117 @@
-return {}
+local Tag    = require "cosy.tag"
+local Data   = require "cosy.data"
+local ignore = require "cosy.util.ignore"
+local http   = require "cosy.util.http"
+local base64 = require "cosy.util.base64"
+local dump   = require "cosy.dump"
+local json   = require "dkjson"
+
+local Cosy = {
+  username = "",
+  password = "",
+  root_directory = os.getenv "HOME" .. "/.cosy/",
+}
+
+local NAME = Tag.NAME
+local cosy = setmetatable ({
+  [NAME] = "cosy"
+}, Cosy)
+
+local tokens = {}
+
+function Cosy:configure (t)
+  self.username = t.username
+  self.password = t.password
+  self.root_directory = t.root_directory
+end
+
+function Cosy:__index (url)
+  -- FIXME: validate url
+  print ("Loading " .. tostring (url))
+  local resource = rawget (cosy, url)
+  if resource then
+    return Data.new (resource)
+  end
+  -- Step 1: ask for edition URL
+  local authorization
+  if Cosy.username and Cosy.password then
+    headers = {
+      Authorization = base64 (Cosy.username .. ":" .. Cosy.password)
+    }
+  end
+  local answer, status = http.request {
+    url     = url,
+    headers = headers,
+  }
+  -- create and load resource:
+  rawset (cosy, url, {})
+  local model = Data.new (cosy) [url]
+  if answer and status == 200 then
+    answer = json.decode (answer)
+    tokens [url] = answer.token
+    self:open_connection (answer.editor)
+  else
+    -- load from file
+    assert (Cosy.root_directory)
+    local lfs = require "lfs"
+    if not lfs.attributes (Cosy.root_directory) then
+      assert (lfs.mkdir (Cosy.root_directory))
+    end
+    local filename = Cosy.root_directory ..
+                     url:gsub ("^https?://", ""):gsub ("/", "-")
+    if not lfs.attributes (filename) then
+      local file = io.open (filename, "w")
+      file:write ("local model = " .. tostring (model) .. "\n")
+      file:close ()
+    end
+    local loader = loadfile (filename)
+    assert (loader)
+    loader ()
+    local file = io.open (filename, "a+")
+    -- set handler to write changed on file
+    local prefix = tostring (model)
+    Data.on_write [Cosy] = function (target, value)
+      local target_str = tostring (target)
+      if model < target then
+        local patch = target_str:gsub (prefix, "model") ..
+                      " = " .. dump (value) .. "\n"
+        file:write (patch)
+        file:flush ()
+      end
+    end
+  end
+  return model
+end
+
+function Cosy:__newindex ()
+  ignore (self)
+  assert (false)
+end
+
+function Cosy:on_connect ()
+end
+
+function Cosy:on_disconnect ()
+end
+
+function Cosy:on_change ()
+end
+
+function Cosy:on_message ()
+end
+
+-- Set global:
+local global = _ENV or _G
+global.cosy = cosy
+global.Cosy = Cosy
+
+-- Load interface:
+if global.js then
+  require "cosy.interface.js"
+--elseif then
+--  require "cosy.interface.socket"
+else
+--  require "cosy.interface.dummy"
+end
+
+return global.cosy
---
+++ src/cosy.test.lua
@@ -0,0 +1,4 @@
+local cosy = require "cosy"
+
+local model = cosy ["http://cosyverif.org/my-model"]
+model.a = 1
