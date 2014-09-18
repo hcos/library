81f01e39cbedaf99091cbab4a7c964db0fe4a5f8
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2014-09-18 23:27:58 +0200

    One step towards interfaces.

--- src/cosy.lua
+++ src/cosy.lua
@@ -1,7 +1,6 @@
 local Tag    = require "cosy.tag"
 local Data   = require "cosy.data"
 local ignore = require "cosy.util.ignore"
-local dump   = require "cosy.dump"
 
 local json   = require "dkjson"
 local base64 = require "ee5_base64"
@@ -30,50 +29,14 @@ else
   interface = require "cosy.interface.c"
 end
 
-local tokens = {}
-
 function Cosy:configure (t)
   self.username = t.username
   self.password = t.password
   self.root_directory = t.root_directory
 end
 
-local function from_server (url)
-  -- TODO
-end
-
-local function from_file (url)
-  local model = Data.new (cosy) [url]
-  assert (Cosy.root_directory)
-  local lfs = require "lfs"
-  -- Create root directory if it does not exist:
-  if not lfs.attributes (Cosy.root_directory) then
-    assert (lfs.mkdir (Cosy.root_directory))
-  end
-  local filename = Cosy.root_directory .. base64.encode (url)
-  -- Create file if it does not exist:
-  if not lfs.attributes (filename) then
-    local file = io.open (filename, "w")
-    file:write ("local model = " .. tostring (model) .. "\n")
-    file:close ()
-  end
-  -- Load file:
-  local loader = loadfile (filename)
-  assert (loader)
-  loader ()
-  -- Set handler to write changed on file
-  local file = io.open (filename, "a+")
-  Data.on_write [Cosy] = function (target, value)
-    if model < target then
-      local patch = tostring (target % model) .. " = " .. dump (value) .. "\n"
-      file:write (patch)
-      file:flush ()
-    end
-  end
-  return model
-end
-
 function Cosy:__index (url)
+  ignore (self)
   if type (url) ~= "string" then
     return nil
   end
@@ -91,10 +54,7 @@ function Cosy:__index (url)
   rawset (cosy, url, {
     [NAME] = "model"
   })
-  --
-  if interface.request then
-    -- ...
-  end
+  local model = Data.new (cosy) [url]
   -- Step 1: ask for edition URL
   local headers
   if Cosy.username and Cosy.password then
@@ -102,38 +62,44 @@ function Cosy:__index (url)
       Authorization = base64.encode (Cosy.username .. ":" .. Cosy.password)
     }
   end
-  local answer, status = interface.request {
-    url     = url,
-    headers = headers,
-  }
-  if answer and status == 200 then
-    -- connect to websocket
-    answer = json.decode (answer)
-    tokens [url] = answer.token
-    -- open websocket connection
-    -- set resource
-    -- fetch model
-    -- set handlers
-  else
-    return from_file (url)
+  do
+    local answer, status = interface.request {
+      url     = url .. "/editor",
+      headers = headers,
+    }
+    -- Try websocket:
+    if answer and status == 200 then
+      answer = json.decode (answer)
+      if pcall (function () interface.dynamic (url, answer) end) then
+        return model
+      end
+    end
   end
+  -- Try to download model:
+  do
+    local answer, status = interface.request {
+      url     = url,
+      headers = headers,
+    }
+    if answer and status == 200 then
+      if pcall (function () interface.static (url, answer) end) then
+        return model
+      end
+    end
+  end
+  -- Try filesystem:
+  do
+    if pcall (function () interface.filesystem (url) end) then
+      return model
+    end
+  end
+  -- Error:
+  assert (false)
 end
 
 function Cosy:__newindex ()
   ignore (self)
   assert (false)
-end
-
-function Cosy:on_connect ()
-end
-
-function Cosy:on_disconnect ()
-end
-
-function Cosy:on_change ()
-end
-
-function Cosy:on_message ()
 end
 
 return global.cosy
