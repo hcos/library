0471171fb585662806f17e2972a9aab12869ebe7
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2014-09-18 10:24:59 +0200

    Add more tests to strings.

--- src/cosy/util/string.test.lua
+++ src/cosy/util/string.test.lua
@@ -1,14 +1,18 @@
 local assert = require "luassert"
 require "cosy.util.string"
 
+-- replace
 do
   assert.are.equal ("" % {}, "")
-end
-
-do
   assert.are.equal ("$a" % { a = 1}, "$a")
+  assert.are.equal ("${a}" % { a = 1 }, "1")
 end
 
+-- quote
 do
-  assert.are.equal ("${a}" % { a = 1 }, "1")
+  assert.are.equal (string.quote "a", '"a"')
+  assert.are.equal (string.quote "[[", '"[["')
+  assert.are.equal (string.quote '"', [['"']])
+  assert.are.equal (string.quote "'", [["'"]])
+  assert.are.equal (string.quote [==['[["]==], [==[[=['[["]=]]==])
 end
