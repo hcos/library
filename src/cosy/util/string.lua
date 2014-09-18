7d3f608382e3ba74db767bd817f14e9367605f5e
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2014-09-18 09:43:49 +0200

    Add string function to test if it is a valid identifier.

--- src/cosy/util/string.lua
+++ src/cosy/util/string.lua
@@ -8,3 +8,24 @@ function string_mt:__mod (parameters)
     end
   )
 end
+
+function string:quote ()
+  if not self:find ('"') then
+    return '"' .. self .. '"'
+  elseif not self:find ("'") then
+    return "'" .. self .. "'"
+  end
+  local pattern = ""
+  while true do
+    if not (   self:find ("%[" .. pattern .. "%[")
+            or self:find ("%]" .. pattern .. "%]")) then
+      return "[" .. pattern .. "[" .. self .. "]" .. pattern .. "]"
+    end
+    pattern = pattern .. "="
+  end
+end
+
+function string:is_identifier ()
+  local i, j = self:find ("[_%a][_%w]*")
+  return i == 1 and j == #self
+end
