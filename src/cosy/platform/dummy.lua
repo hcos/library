8e86902d918b6c14660fdca3ff290536a6fb6943
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2014-09-25 11:35:48 +0200

    Add dummy platform for debugging.

---
+++ src/cosy/platform/dummy.lua
@@ -0,0 +1,54 @@
+local _          = require "cosy.util.string"
+local ignore     = require "cosy.util.ignore"
+local logging   = require "logging"
+logging.console = require "logging.console"
+
+local logger = logging.console "%level %message\n"
+
+local Platform = {}
+
+Platform.__index = Platform
+
+function Platform:log (message)
+  ignore (self)
+  logger:debug (message)
+end
+
+function Platform:info (message)
+  ignore (self)
+  logger:info (message)
+end
+
+function Platform:warn (message)
+  ignore (self)
+  logger:warn (message)
+end
+
+function Platform:error (message)
+  ignore (self)
+  logger:error (message)
+end
+
+function Platform:send (message)
+end
+
+-- Cosy.models [url] = {
+--   [VALUE] = <model>
+--   username = ...
+--   password = ...
+--   resource = ...
+--   editor   = ...
+--   editor.token = ...
+-- }
+--
+
+function Platform.new (meta)
+  return setmetatable ({
+    meta = meta,
+  }, Platform)
+end
+
+function Platform:close ()
+end
+
+return Platform
