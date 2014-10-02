403d68916f9d23b301902b624aa97ad446ee09de
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2014-10-02 12:35:21 +0200

    Improve the Patches object.

--- src/cosy/patches.lua
+++ src/cosy/patches.lua
@@ -11,7 +11,7 @@ function Patches.new ()
   }, Patches)
 end
 
-function Patches:insert (p)
+function Patches:push (p)
   assert (type (p) == "table")
   local ids     = self.ids
   local patches = self.patches
@@ -19,6 +19,40 @@ function Patches:insert (p)
   p.id = #ids + 1
   ids [#ids + 1] = p
   return p
+end
+
+function Patches:pop ()
+  local ids      = self.ids
+  local patches  = self.patches
+  local patch    = patches [1]
+  ids [patch.id] = nil
+  table.remove (patches, 1)
+end
+
+function Patches:__ipairs ()
+  return coroutine.wrap (
+    function ()
+      for i, p in ipairs (self.patches) do
+        coroutine.yield (i, p)
+      end
+    end
+  )
+end
+
+function Patches:__len ()
+  return #(self.patches)
+end
+
+function Patches:__index (n)
+  if type (n) == "number" then
+    return self.patches [n]
+  else
+    return Patches [n]
+  end
+end
+
+function Patches:__newindex ()
+  assert (false)
 end
 
 return Patches
