a047dbc3550fa3aa46e9b5440a830bddc7503deb
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2014-10-02 16:19:14 +0200

    Test

--- src/cosy/platform/ev.lua
+++ src/cosy/platform/ev.lua
@@ -1,15 +1,12 @@
 local json       = require "dkjson"
 local _          = require "cosy.util.string"
 local ignore     = require "cosy.util.ignore"
-local http       = require "socket.http"
-local https      = require "ssl.https"
+local websocket  = require "websocket"
+local ev         = require "ev"
 
 local logging    = require "logging"
 logging.console  = require "logging.console"
-
-local GLOBAL = _G or _ENV
-local cosy = GLOBAL.cosy
-local meta = GLOBAL.meta
+local logger     = logging.console "%level %message\n"
 
 local Platform = {}
 
@@ -36,102 +33,80 @@ function Platform:error (message)
 end
 
 function Platform:send (message)
+  for k in pairs (self.websocket) do
+    print (k)
+  end
   if self.websocket.readyState == 1 then
-    message.token = self.token
     self.websocket:send (json.encode (message))
   else
     self:log ("Unable to send: " .. json.encode (message))
   end
 end
 
-local function load_url (url)
-  local body
-  local status
-  if url:find ("http://") == 1 then
-    body, status = http.request (url)
-  elseif url:find ("https://") == 1 then
-    body, status = https.request (url)
-  else
-    return nil
-  end
-  -- The status code can be:
-  -- * 200 (OK)
-  -- * 3xx (Redirect)
-  -- Otherwise, there is an error.
-  if status ~= 200 and status < 300 or status >= 400 then
-    error ("Cannot fetch " .. url)
-  end
-  return body
-end
-
 function Platform.new (meta)
-  -- TODO: cross-domain + authentication
-  local username = meta.editor.username ()
-  local password = meta.editor.password ()
-  local url      = meta.editor.url ()
-  if username then
-    url = url:gsub ("^https://", "https://${username}:${password}@" % {
-      username = username,
-      password = password
-    })
-  end
-  local editor_info = load_url (url)
-  if not editor_info then
-    Platform:warn ("Cannot get editor ${editor_url} from repository." % {
-      editor_url = meta.editor.url ()
-    })
-    return setmetatable ({
-      meta = meta,
-    }, Platform)
-  end
-  editor_info = json.decode (editor_info)
-  meta.editor.token = editor_info.token
-  meta.editor.url   = editor_info.url
   local websocket = websocket.client.ev { timeout = 2 }
-  websocket:connect (editor_info.url, "cosy")
+  local protocol  = meta.protocol
+  local platform  = setmetatable ({
+    meta      = meta,
+    websocket = websocket,
+  }, Platform)
+  websocket:connect (meta.editor, "cosy")
   websocket:on_open (function ()
+    protocol:on_open ()
   end)
   websocket:on_close (function ()
+    protocol:on_close ()
   end)
   websocket:on_message (function (_, message)
     if not message then
       return
     end
+    protocol:on_message (json.decode (message.data))
   end)
   websocket:on_error (function (_, err)
+    logger:warn ("Error received: ${err}." % {
+      err = err
+    })
+    websocket:close ()
   end)
-  --[[
-  function ws.execute (f, again)
-    local co = coroutine.create (function ()
-      local ok, err = pcall (f)
-      if not ok then
-        print (err)
-      end
-    end)
-    local timer = ev.Timer.new (function (_, timer, _)
-      local _, delay = coroutine.resume (co)
-      if coroutine.status (co) == "dead" then
-        timer:stop (ev.Loop.default)
-      else
-        timer:again (ev.Loop.default, delay)
-      end
-    end, 1)
-    timer:start (ev.Loop.default)
-  end
-  function ws.loop ()
-    ev.Loop.default:loop ()
-  end
-  function ws.stop ()
-    ev.Loop.default:unloop ()
-  end
-  --]]
-  return setmetatable ({
-    meta      = meta,
-    websocket = websocket,
-  }, Platform)
+  return platform
 end
 
 function Platform:close ()
+  if self.websocket then
+    self.websocket:close ()
+    self.websocket = nil
+  end
 end
 
-return connect
+function Platform:execute (f, again)
+  ignore (self, again)
+  local co = coroutine.create (function ()
+    local ok, err = pcall (f)
+    if not ok then
+      logger:warn (err)
+    end
+  end)
+  local timer = ev.Timer.new (function (_1, timer, _2)
+    ignore (_1, _2)
+    local _, delay = coroutine.resume (co)
+    if coroutine.status (co) == "dead" then
+      timer:stop (ev.Loop.default)
+    else
+      timer:again (ev.Loop.default, delay)
+    end
+  end, 1)
+  timer:start (ev.Loop.default)
+end
+
+function Platform:loop ()
+  ignore (self)
+  ev.Loop.default:loop ()
+end
+
+function Platform:stop ()
+  ignore (self)
+  ev.Loop.default:unloop ()
+end
+
+return Platform
