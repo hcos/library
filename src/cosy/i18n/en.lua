629fd0eb1d97f902c09b8652dfb1030c4baa698e
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2015-01-21 22:18:28 +0100

    Add namespace to i18n messages.

--- src/cosy/i18n/en.lua
+++ src/cosy/i18n/en.lua
@@ -1,12 +1,26 @@
 return {
-  available_dependency  = "%{component} is available using '%{dependency}'",
-  missing_dependency    = "%{component} is not available",
-  available_locale      = "i18n locale '%{locale}' has been loaded",
-  available_compression = "compression '%{compression}' has been loaded",
-  bcrypt_rounds         = {
+  ["platform:available-dependency"] =
+    "%{component} is available using '%{dependency}'",
+  ["platform:missing-dependency"] =
+    "%{component} is not available",
+  ["platform:available-locale"] =
+    "i18n locale '%{locale}' has been loaded",
+  ["platform:available-compression"] =
+    "compression '%{compression}' has been loaded",
+  ["platform:bcrypt-rounds"] = {
     one   = "using one round in bcrypt for at least %{time} milliseconds of computation",
     other = "using %{count} rounds in bcrypt for at least %{time} milliseconds of computation",
   },
-  configuration_conflict = "directory %{path} contains several configuration files, instead of just one",
-  configuration_using    = "using configuration in directory %{path}",
+  ["configuration:conflict"] =
+    "directory %{path} contains several configuration files, instead of just one",
+  ["configuration:using"] =
+    "using configuration in directory %{path}",
+  ["turing:what-is-round"] =
+    "What is represented as a circle in Petri nets: a *place* or a *transition*?",
+  ["turing:what-is-rectangular"] =
+    "What is represented as a rectangle in Petri nets: a *place* or a *transition*?",
+  ["place"] =
+    "place",
+  ["transition"] =
+    "transition",
 }
