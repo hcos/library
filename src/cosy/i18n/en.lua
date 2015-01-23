c66b23f65769219d955495d4e7e67b55f0fde0e8
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2015-01-23 12:50:48 +0100

    Add messages for parameters checking and authentication.

--- src/cosy/i18n/en.lua
+++ src/cosy/i18n/en.lua
@@ -23,4 +23,28 @@ return {
     "place",
   ["transition"] =
     "transition",
+  ["check:error"] =
+    "some parameters are invalid or missing",
+  ["check:username:is-string"] =
+    "a username must be a string",
+  ["check:username:non-empty"] =
+    "a username must be non empty",
+  ["check:username:alphanumeric"] =
+    "a username must contain only alphanumeric characters",
+  ["check:password:is-string"] =
+    "a password must be a string",
+  ["check:password:min-size"] = {
+    one   = "a (trimmed) password must contain at least one character",
+    other = "a (trimmed) password must be at least %{count} characters long",
+  },
+  ["check:email:pattern"] =
+    "an email address must comply to the standard",
+  ["authenticate:non-existing"] =
+    "authentication failed, because the given user does not exist",
+  ["authenticate:non-user"] =
+    "authentication failed, because the given username is not a user",
+  ["authenticate:erroneous"] =
+    "authentication failed, because of an erroneous username/password couple",
+  ["authenticate:cheap-password"] =
+    "password for %{username} is hashed using too few rounds and thus rehashed",
 }
