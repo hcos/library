18766a857c02a0490b6c4fc71c8c86e42bb8bf87
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2015-01-29 14:11:35 +0100

    Add more messages, and license draft.

--- src/cosy/i18n/en.lua
+++ src/cosy/i18n/en.lua
@@ -1,4 +1,46 @@
 return {
+  ["license"] =
+[[
+Cas n°1: Utilisation du logiciel Cosyverif par téléchargement
+
+If you download the Cosyverif software, you acknowledge that this software is distributed under the MIT license and that you accept its terms.
+If you decline the terms of this license you must not download this software.
+
+Cas n°2: Utilisation du logiciel Cosyverif directement sur le site
+
+If you use the Cosyverif software, you acknowledge that this software is distributed under the MIT license and you accept its terms.
+If you decline the terms of this license you must not use this software.
+
+CAS 3:
+- Pour le cas que quelqu'un souhaite contribuer avec des modules, c'est moins facile. 
+ est-ce que celui qui propose le module vous donne une licence ?  et si oui, avec quels droits ?
+
+Cas 3a: A l'intention du contributeur qui veut ajouter une contribution au logiciel Cosyverif
+
+If you want that your contribution be integrated into the Cosyverif software be sure to read and accept the following provisions:
+
+By integrating your contribution into the Cosyverif software, you warrant that you are the author of the contribution and that the latter does not infringe the intellectual property rights of a third party.
+
+You assign your patrimonial rights to X for the duration of the patrimonial rights, for all territories throughout the world in order to distribute your contribution as a module of the Cosyverif software.
+It is specified that the rights assigned to X are as follows:
+- the right to reproduce the contribution in a whole,........
+- the right to represent your contribution in a whole,.....
+- the right to distribute your contribution under the MIT License
+- the right to integrate your contribution into the Cosyverif software
+
+This assignment of rights is granted for free.
+
+Cas 3b: A l'intention des utilisateurs quand la contribution est intégrée au logiciel Cosyverif sans avoir été préalablement accepté par les administrateurs réseaux
+
+Be aware the Cosyverif software contains several modules provided "as is", we do not warrant that the modules do not infringe the intellectual property rigths of a third party.
+]],
+  ["license:reject"] =
+    "license %{digest} is rejected by user %{username}",
+  ["license:accept"] =
+    "license %{digest} is accepted by user %{username}",
+  ["license:accept?"] =
+    "accepting the license is required",
+
   ["platform:available-dependency"] =
     "%{component} is available using '%{dependency}'",
   ["platform:missing-dependency"] =
@@ -36,7 +78,7 @@ return {
     one   = "a %{key} must contain at most one character",
     other = "a %{key} must be at most %{count} characters long",
   },
-  
+
   ["check:username:alphanumeric"] =
     "a username must contain only alphanumeric characters",
   ["check:email:pattern"] =
@@ -52,4 +94,12 @@ return {
     "authentication failed, because of an erroneous username/password couple",
   ["authenticate:cheap-password"] =
     "password for %{username} is hashed using too few rounds and thus rehashed",
+
+  ["create-user:connected-already"] =
+    "an already connected user cannot create a user account",
+  ["create-user:email-already"] =
+    "email address %{email} is already bound to user account %{username}",
+  ["create-user:username-already"] =
+    "username %{username} is already a user account",
+
 }
