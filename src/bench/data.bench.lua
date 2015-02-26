0b3e1469b3aa97c9a80c039204dce03d45b7e557
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2015-02-26 16:49:54 +0100

    Improve data benchmark.

--- src/bench/data.bench.lua
+++ src/bench/data.bench.lua
@@ -1,10 +1,11 @@
+local profiler = false
+local quantity = 1000000
 local assert   = require "luassert"
 local Data     = require "cosy.data"
 local Platform = require "cosy.platform"
 
-do
+local function test_write ()
   local repository = Data.new {}
-  local quantity = 100000
   collectgarbage ()
   local start    = Platform.time ()
   local depends  = {}
@@ -21,62 +22,67 @@ do
   print ("# create / second:", math.floor (quantity / (finish - start)))
 end
 
-do
-  local repository = Data.new {}
-  local quantity = 100000
-  Data.options (repository) .on_write = function () end
-  collectgarbage ()
-  local start    = Platform.time ()
-  local depends  = {}
-  for i = 1, quantity do
-    repository [i] = {
-      [i] = i,
-    }
-    depends [#depends+1] = repository [i]
-  end
-  repository.all = {
-    [Data.DEPENDS] = depends
-  }
-  local finish   = Platform.time ()
-  print ("# create / second:", math.floor (quantity / (finish - start)))
-end
-
-for _, width in ipairs {
-  1, 2, 5, 10, 20,
-} do
-  for _, depth in ipairs {
+local function test_read ()
+  for _, width in ipairs {
     1, 2, 5, 10, 20,
   } do
-    local repository = Data.new {}
-    local quantity = 100000
-    repository [0] = {}
-    for i = 1, width do
-      local t = {}
-      local current = t
-      for j = 1, depth do
-        current [j] = {}
-        current     = current [j]
+    for _, depth in ipairs {
+      1, 2, 5, 10, 20,
+    } do
+      local repository = Data.new {}
+      repository [0] = {}
+      for i = 1, width do
+        local t = {}
+        local current = t
+        for j = 1, depth do
+          current [j] = {}
+          current     = current [j]
+        end
+        current._ = i % width + 1
+        repository [i] = {
+          [i] = t,
+          [Data.DEPENDS] = { repository [i-1] },
+        }
       end
-      current._ = i % width + 1
-      repository [i] = {
-        [i] = t,
-        [Data.DEPENDS] = { repository [i-1] },
+      repository.all = {
+        [Data.DEPENDS] = { repository [width] }
       }
-    end
-    repository.all = {
-      [Data.DEPENDS] = { repository [width] }
-    }
-    collectgarbage ()
-    local start    = Platform.time ()
-    for i = 1, quantity do
-      local d = repository.all [i % width + 1]
-      for j = 1, depth do
-        d = d [j]
+      collectgarbage ()
+      local start    = Platform.time ()
+      for i = 1, quantity do
+        local d = repository.all [i % width + 1]
+        for j = 1, depth do
+          d = d [j]
+        end
+        local _ = d._
+  --      assert.are.equal (d._, i % width + 1)
       end
-      local _ = d._
---      assert.are.equal (d._, i % width + 1)
+      local finish   = Platform.time ()
+      print ("# read / second / depth - width", depth, width, math.floor (quantity / (finish - start)))
+--[[
+      local memory = math.ceil (collectgarbage ("count")/1024)
+      print ("Memory         : " .. tostring (memory) .. " Mbytes.")
+      local duration = finish - start
+      local average_time = math.floor (quantity / duration)
+      print ("Average time   : " .. tostring (average_time) .. " operations / second.")
+      local average_memory = math.floor (memory*1024*1024 / quantity)
+      print ("Average memory : " .. tostring (average_memory) .. " bytes / object.")
+--]]
     end
-    local finish   = Platform.time ()
-    print ("# read / second / depth - width", depth, width, math.floor (quantity / (finish - start)))
   end
 end
+
+for _, f in ipairs {
+  test_write,
+  test_read,
+} do
+  if profiler then
+    profiler = require "profiler"
+    profiler:start "data.out"
+  end
+  f ()
+  if profiler then
+    profiler:stop()
+  end
+end
+
