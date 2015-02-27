a7d6c52d192574c167ae71efb3c6d5e123859496
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2015-02-27 23:11:16 +0100

    Depend on platform, add tests.

--- src/cosy/string.lua
+++ src/cosy/string.lua
@@ -1,5 +1,12 @@
-local string_metatable = getmetatable ""
-string_metatable.__mod = require "i18n.interpolate"
+local Platform = require "cosy.platform"
+
+local metatable = getmetatable ""
+
+metatable.__mod = Platform.i18n.interpolate
+--    > require "cosy.string"
+--    > print ("%{key}" % { key = "some text" })
+--    ...
+--    some text
 
 function string:quote ()
   if not self:find ('"') then
@@ -16,13 +23,48 @@ function string:quote ()
     pattern = pattern .. "="
   end
 end
+--    > require "cosy.string"
+--    > local text = "abc"
+--    > print (text:quote ())
+--    ...
+--    "abc"
+--    > local text = [[a"bc]]
+--    > print (text:quote ())
+--    ...
+--    'a"bc'
+--    > local text = [[a'bc]]
+--    > print (text:quote ())
+--    ...
+--    "a'bc"
+--    > local text = [[a"b'c]]
+--    > print (text:quote ())
+--    ...
+--    [[a"b'c]]
+--    > local text = [=[a[["b']]c]=]
+--    > print (text:quote ())
+--    ...
+--    [=[a[["b']]c]=]
 
 function string:is_identifier ()
   local i, j = self:find ("[_%a][_%w]*")
   return i == 1 and j == #self
 end
+--    > require "cosy.string"
+--    > local text = "abc"
+--    > print (text:is_identifier ())
+--    ...
+--    true
+--    > local text = "0abc"
+--    > print (text:is_identifier ())
+--    ...
+--    false
 
 -- http://lua-users.org/wiki/StringTrim
 function string:trim ()
   return self:match "^()%s*$" and "" or self:match "^%s*(.*%S)"
 end
+--    > require "cosy.string"
+--    > local text = "   abc   def   "
+--    > print (text:trim ())
+--    ...
+--    abc   def
