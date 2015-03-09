fbfa6155c7fb4cf0797759f3ffc4b3b4d4af4297
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2015-03-09 15:53:05 +0100

    Remove useless files.

--- src/cosy.test.lua
+++
@@ -1,6 +0,0 @@
-local cosy = require "cosy.platform.dummy"
-
-local model = cosy ["http://cosyverif.org/my-model"]
-model.a = 1
-local _ = model.c
-model.b = cosy ["http://cosyverif.org/other-model"].c
