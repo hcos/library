227c8e8f9c3272899804f9f437b72b02b329ddce
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2015-03-09 15:44:20 +0100

    Websocket client as test.

--- src/test.lua
+++ src/test.lua
@@ -1,113 +1,177 @@
-require "compat52"
-local serpent = require "serpent"
+local Configuration = require "cosy.configuration" .whole
+local Platform      = require "cosy.platform"
+                      require "cosy.string"
+local copas         = require "copas"
+local websocket     = require "websocket"
 
+local nb_threads    = 10
+local nb_iterations = 10
 
-local PARENTS = "parents"
-local LAYERS  = "layers"
+local running  = 0
+local closed   = 0
+local sent     = {}
+local received = {}
 
--- https://xivilization.net/~marek/blog/2014/12/08/implementing-c3-linearization/
-local function c3 (data)
-  if #(data [PARENTS]) == 0 then
-    data [LAYERS] = { data }
-  else
-    local n  = { 1 }
-    local l  = { { data } }
-    local parents = data [PARENTS]
-    for i = #parents, 1, -1 do
-      local parent = parents [i]
-      local layers = parent  [LAYERS]
-      if layers then
-        l [#l+1] = layers
-        n [#n+1] = #layers
-      end
+do
+  local redis     = require "redis"
+  local host      = Configuration.redis.host._
+  local port      = Configuration.redis.port._
+  local database  = Configuration.redis.database._
+  local client    = redis.connect (host, port)
+  client:select (database)
+  client:flushall ()
+end
+
+for thread = 1, nb_threads  do
+  sent     [thread] = 0
+  received [thread] = 0
+  copas.addthread (function ()
+    local client = websocket.client.copas {
+      timeout = 5
+    }
+    local url = "ws://%{host}:%{port}" % {
+      host = Configuration.server.host._,
+      port = tonumber (Configuration.server.port._),
+    }
+    client:connect (url, "cosy")
+    running = running + 1
+    
+    local information
+    do
+      client:send (Platform.table.encode {
+        version = "2.0",
+        method  = "information",
+        params  = {
+          token   = nil,
+          request = nil,
+        },
+        id = "inforamtion",
+      })
+      local message = client:receive ()
+      local _, result = Platform.table.decode (message)
+      information = result.result
     end
-    l [#l+1] = parents
-    n [#n+1] = #parents
-    local tails  = {}
-    for i = 1, #l do
-      local v = l [i]
-      for j = 1, #v do
-        local w   = v [j]
-        tails [w] = (tails [w] or 0) + 1
-      end
+    local tos
+    do
+      client:send (Platform.table.encode {
+        version = "2.0",
+        method  = "tos",
+        params  = {
+          token   = nil,
+          request = nil,
+        },
+        id = "tos",
+      })
+      local message = client:receive ()
+      local _, result = Platform.table.decode (message)
+      tos = result.result
     end
-    local layers = {}
-    while #l ~= 0 do
-      for i = 1, #l do
-        local vl, vn  = l [i], n [i]
-        local first   = vl [vn]
-        tails [first] = tails [first] - 1
-      end
-      local head
-      for i = 1, #l do
-        local vl, vn = l [i], n [i]
-        local first  = vl [vn]
-        if tails [first] == 0 then
-          head = first
+    local validation
+    do
+      client:send (Platform.table.encode {
+        version = "2.0",
+        method  = "create-user",
+        params  = {
+          token   = nil,
+          request = {
+            username   = "user-" .. tostring (thread),
+            password   = "password",
+            email      = "user-" .. tostring (thread) .. "@machin.fr",
+            tos_digest = tos.tos_digest,
+            locale     = "en",
+            name       = "User " .. tostring (thread),
+          },
+        },
+        id = "create-user",
+      })
+      local message = client:receive ()
+      local _, result = Platform.table.decode (message)
+      validation = result.result.token
+    end
+    do
+      client:send (Platform.table.encode {
+        version = "2.0",
+        method  = "activate-user",
+        params  = {
+          token   = validation,
+          request = nil,
+        },
+        id = "activate-user",
+      })
+      local message = client:receive ()
+      local _, result = Platform.table.decode (message)
+    end
+    local token
+    do
+      client:send (Platform.table.encode {
+        version = "2.0",
+        method  = "authenticate",
+        params  = {
+          token   = nil,
+          request = {
+            username = "user-" .. tostring (thread),
+            password = "password",
+          },
+        },
+        id = "authenticate",
+      })
+      local message = client:receive ()
+      local _, result = Platform.table.decode (message)
+      token = result.result.token
+    end
+    for id = 1, nb_iterations do
+      copas.addthread (function ()
+         local client = websocket.client.copas {
+          timeout = 5
+        }
+        local url = "ws://%{host}:%{port}" % {
+          host = Configuration.server.host._,
+          port = tonumber (Configuration.server.port._),
+        }
+        client:connect (url, "cosy")
+        client:send (Platform.table.encode {
+              version = "2.0",
+          method  = "information",
+          params  = {
+            token   = token,
+            request = nil,
+          },
+          id = tostring (id),
+        })
+        sent     [thread] = sent     [thread] + 1
+        local message = client:receive ()
+        received [thread] = received [thread] + 1
+      end)
+    end
+    copas.addthread (function ()
+      while true do
+        if sent [thread] == nb_iterations then
+          copas.sleep (1)
+          client:close ()
+          closed = closed + 1
+          print ("closed", closed)
           break
         end
+        copas.sleep (0.01)
       end
-      if head == nil then
-        error "Linearization failed."
-      end
-      layers [#layers + 1] = head
-      for i = 1, #l do
-        local vl, vn = l [i], n [i]
-        local first  = vl [vn]
-        if first == head then
-          n [i] = n [i] - 1
-        else
-          tails [first] = tails [first] + 1
-        end
-      end
-      local nl, nn = {}, {}
-      for i = 1, #l do
-        if n [i] ~= 0 then
-          nl [#nl+1] = l [i]
-          nn [#nn+1] = n [i]
-        end
-      end
-      l, n = nl, nn
-    end
-    for i = 1, #layers / 2 do
-      layers [i], layers [#layers-i+1] = layers [#layers-i+1], layers [i]
-    end
-    data [LAYERS] = layers
-  end
+    end)
+  end)
 end
 
-local function class (name, parents)
-  local ps = {}
-  if parents then
-    for i = 1, #parents do
-      ps [i] = parents [#parents-i+1]
+copas.addthread (function ()
+  while true do
+    local r = 0
+    for _, v in pairs (received) do
+      r = r + v
+    end
+    if closed == nb_threads then
+      print ("received", r * 100 / (nb_threads * nb_iterations), "%")
+      os.exit (0)
+    else
+      print ("received", r * 100 / (nb_threads * nb_iterations), "%")
+      copas.sleep (0.1)
     end
   end
-  local data = setmetatable ({
-    [PARENTS] = ps,
-  }, {
-    __tostring = function (x) return name end,
-  })
---  print ("===== " .. tostring (data) .. " =====")
-  c3 (data)
---  print ("parents:", serpent.dump (data [PARENTS]))
---  print ("layers :", serpent.dump (data [LAYERS ]))
-  return data
-end
+end)
 
-local profiler = require "profiler"
-
-profiler.start "test.log"
-for i = 1, 5000 do
-  local o = class ("o")
-  local a = class ("a", { o })
-  local b = class ("b", { o })
-  local c = class ("c", { o })
-  local d = class ("d", { o })
-  local e = class ("e", { o })
-  local k1 = class ("k1", { a, b, c })
-  local k2 = class ("k2", { d, b, e })
-  local k3 = class ("k3", { d, a })
-  local z = class ("z", { k1, k2, k3 })
-end
-profiler.stop ()
+copas.loop ()
