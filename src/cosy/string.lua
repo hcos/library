c571e80eb353a18d29e191ef0076d97b4f39f72c
Alban Linard <alban.linard@lsv.ens-cachan.fr>
2015-04-15 14:33:24 +0200

    Fix string manipulation module.

--- src/cosy/string.lua
+++ src/cosy/string.lua
@@ -1,70 +1,112 @@
-local Platform = require "cosy.platform"
-
 local metatable = getmetatable ""
 
-metatable.__mod = Platform.i18n.interpolate
 --    > require "cosy.string"
---    > print ("%{key}" % { key = "some text" })
---    ...
---    some text
 
-function string:quote ()
-  if not self:find ('"') then
-    return '"' .. self .. '"'
-  elseif not self:find ("'") then
-    return "'" .. self .. "'"
-  end
-  local pattern = ""
-  while true do
-    if not (   self:find ("%[" .. pattern .. "%[")
-            or self:find ("%]" .. pattern .. "%]")) then
-      return "[" .. pattern .. "[" .. self .. "]" .. pattern .. "]"
+-- taken from i18n/interpolate
+metatable.__mod = function (pattern, variables)
+  variables = variables or {}
+  return pattern:gsub ("(.?)%%{(.-)}", function (previous, key)
+    if previous == "%" then
+      return
     end
-    pattern = pattern .. "="
-  end
+    local value = tostring (variables [key])
+    return previous .. value
+  end)
 end
---    > require "cosy.string"
---    > local text = "abc"
---    > print (text:quote ())
---    ...
---    "abc"
---    > local text = [[a"bc]]
---    > print (text:quote ())
---    ...
---    'a"bc'
---    > local text = [[a'bc]]
---    > print (text:quote ())
---    ...
---    "a'bc"
---    > local text = [[a"b'c]]
---    > print (text:quote ())
---    ...
---    [[a"b'c]]
---    > local text = [=[a[["b']]c]=]
---    > print (text:quote ())
---    ...
---    [=[a[["b']]c]=]
 
-function string:is_identifier ()
-  local i, j = self:find ("[_%a][_%w]*")
-  return i == 1 and j == #self
+--    > = "%{_1}-%{_2}-%{_3}" % {
+--    >     _1 = "abc",
+--    >     _2 = true,
+--    >     _3 = nil,
+--    >   }
+--    "abc-true-nil"
+
+--    > = "%%{_1}-%%{_2}-%%{_3}" % {
+--    >     _1 = "abc",
+--    >     _2 = true,
+--    >     _3 = nil,
+--    >   }
+--    "%%{_1}-%%{_2}-%%{_3}"
+
+-- http://stackoverflow.com/questions/9790688/escaping-strings-for-gsub
+function string.escape (string)
+  return string
+        :gsub('%%', '%%%%')
+        :gsub('%^', '%%%^')
+        :gsub('%$', '%%%$')
+        :gsub('%(', '%%%(')
+        :gsub('%)', '%%%)')
+        :gsub('%.', '%%%.')
+        :gsub('%[', '%%%[')
+        :gsub('%]', '%%%]')
+        :gsub('%*', '%%%*')
+        :gsub('%+', '%%%+')
+        :gsub('%-', '%%%-')
+        :gsub('%?', '%%%?')
 end
---    > require "cosy.string"
+
+metatable.__div = function (pattern, string)
+  local names = {}
+  pattern = pattern:escape ()
+  pattern = pattern:gsub ("(.?)%%%%{(.-)}", function (previous, key)
+    if previous == "%" then
+      return
+    end
+    names [#names+1] = key
+    return previous .. "(.*)"
+  end)
+  if pattern:sub (1,1) ~= "^" then
+    pattern = "^" .. pattern
+  end
+  if pattern:sub (-1,1) ~= "$" then
+    pattern = pattern .. "$"
+  end
+  local results = { string:match (pattern) }
+  if #results == 0 then
+    return nil
+  end
+  local result  = {}
+  for i = 1, #names do
+    result [names [i]] = results [i]
+  end
+  return result
+end
+
+--    > = "abc%{key}xyz" / "abcdefxyz"
+--    { key = "def" }
+
+--    > = "abc%{key}xyz" / "abcdefijk"
+--    nil
+
+--    > = "abc%%{key}xyz" / "abcdefxyz"
+--    nil
+
+function string.quote (s)
+  return string.format ("%q", s)
+end
+
 --    > local text = "abc"
---    > print (text:is_identifier ())
---    ...
+--    > = text:quote ()
+--    [["abc"]]
+
+function string.is_identifier (s)
+  local i, j = s:find ("[_%a][_%w]*")
+  return i == 1 and j == #s
+end
+
+--    > local text = "abc"
+--    > = text:is_identifier ()
 --    true
+
 --    > local text = "0abc"
---    > print (text:is_identifier ())
---    ...
+--    > = text:is_identifier ()
 --    false
 
 -- http://lua-users.org/wiki/StringTrim
-function string:trim ()
-  return self:match "^()%s*$" and "" or self:match "^%s*(.*%S)"
+function string.trim (s)
+  return s:match "^()%s*$" and "" or s:match "^%s*(.*%S)"
 end
---    > require "cosy.string"
+
 --    > local text = "   abc   def   "
---    > print (text:trim ())
---    ...
---    abc   def
+--    > = text:trim ()
+--    "abc   def"
