9081c5ec91259f9347d833ab5b750eb544ea416c
Alban Linard <alban@linard.fr>
2015-05-19 10:13:17 +0200

    Add --clean option to clean redis database before run.

--- src/cosy.lua
+++ src/cosy.lua
@@ -7,10 +7,28 @@ local loader  = require "cosy.loader"
 local cli     = loader "cliargs"
 
 cli:set_name (arg [0])
+
+cli:add_option (
+  "-c, --clean",
+  "clean redis database"
+)
+
 local args = cli:parse_args ()
+
 if not args then
   cli:print_help ()
   os.exit (1)
+end
+
+if args.clean then
+  local redis     = loader "redis"
+  local host      = loader.configuration.redis.host._
+  local port      = loader.configuration.redis.port._
+  local database  = loader.configuration.redis.database._
+  local client = redis.connect (host, port)
+  client:select (database)
+  client:flushdb ()
+  package.loaded.redis = nil
 end
 
 loader.server.start ()
