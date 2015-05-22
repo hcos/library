a2eb0d506ac23eeb9b4924dd03ddd84c4657e4a5
sofiane <osboxes@osboxes>
2015-05-22 13:35:07 +0200

    library.lua remove console log

--- src/cosy/library.lua
+++ src/cosy/library.lua
@@ -7,7 +7,6 @@ local Client  = {}
 if _G.js then
   function Client.__index (client, operation)
     return function (parameters, try_only)
-     --js.global.console:log ("client waiting" .. tostring (client._waiting))
       local identifier = #client._waiting+1
       client._ws:send (Value.expression {
         identifier = identifier,
@@ -15,8 +14,7 @@ if _G.js then
         parameters = parameters or {},
         try_only = try_only,
       })
-       js.global.console:log ("wait: index")
-       client._waiting[identifier] = coroutine.running ()
+      client._waiting[identifier] = coroutine.running ()
       local result = coroutine.yield ()
       if result == nil then
         error {
@@ -38,11 +36,9 @@ if _G.js then
     }, Client)
     client._ws = _G.js.new (_G.js.global.WebSocket, url, "cosy")
     client._ws.onopen    = function (ws)
-      js.global.console:log ("connect")
       coroutine.resume (co, true)
     end
     client._ws.onclose   = function (ws,event)
-      js.global.console:log ("disconnect")
       coroutine.resume (co, nil, event.reason)
     end
     client._ws.onmessage = function (ws,event)
@@ -57,7 +53,6 @@ if _G.js then
         client.on_update ()
       end
     end
-    js.global.console:log ("wait: client")
     local ok, err = coroutine.yield ()
     if not ok then
       error (err)
