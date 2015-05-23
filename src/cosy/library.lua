adb7ec786f4789aa3f0d68e4cce49f31c968485b
Alban Linard <alban@linard.fr>
2015-05-23 23:21:35 +0200

    Fix library for native and javascript.

--- src/cosy/library.lua
+++ src/cosy/library.lua
@@ -1,6 +1,7 @@
 local Configuration = require "cosy.configuration"
 local Digest        = require "cosy.digest"
 local Value         = require "cosy.value"
+local Scheduler     = require "cosy.scheduler"
 
 local Library = {}
 local Client  = {}
@@ -27,169 +28,126 @@ local function patch (client)
   return client
 end
 
-if _G.js then
-
-  function Client.__index (client, operation)
-    local result = function (parameters, try_only)
+function Client.__index (client, operation)
+  return function (parameters, try_only)
+    local result
+    local f = function ()
+      local co         = coroutine.running ()
       local identifier = #client._waiting + 1
+      client._waiting [identifier] = co
+      client._results [identifier] = nil
       client._ws:send (Value.expression {
         identifier = identifier,
         operation  = operation,
         parameters = parameters or {},
         try_only   = try_only,
       })
-      client._waiting [identifier] = coroutine.running ()
-      local result = coroutine.yield ()
-      if result == nil then
-        error {
-          _ = "client:timeout",
-        }
-      elseif result.success then
-        return result.response
-      else
-        error (result.error)
-      end
+      Scheduler.sleep (Configuration.client.timeout._)
+      result = client._results [identifier]
+      client._waiting [identifier] = nil
+      client._results [identifier] = nil
     end
-    return result
+    Scheduler.addthread (f)
+    Scheduler.loop ()
+    if result == nil then
+      return nil, {
+        _ = "client:timeout",
+      }
+    elseif result.success then
+      return result.response or true
+    else
+      return nil, result.error
+    end
   end
+end
 
-  function Library.client (url)
-    local client = setmetatable ({
-      _results = {},
-      _waiting = {},
-    }, Client)
-    client._ws   = _G.js.new (_G.js.global.WebSocket, url, "cosy")
-    client._ws.onopen    = function ()
-      coroutine.resume (coroutine.running (), true)
-    end
-    client._ws.onclose   = function (_, event)
-      coroutine.resume (coroutine.running (), nil, event.reason)
-    end
-    client._ws.onmessage = function (_, event)
-      local message    = Value.decode (event.data)
-      local identifier = message.identifier
-      if identifier then
-        local co = client._waiting [identifier]
-        assert (co)
-        client._waiting[identifier] = nil
-        coroutine.resume (co, message)
-      else
-        client.on_update ()
-      end
-    end
-    local ok, err = coroutine.yield ()
-    if not ok then
-      error (err)
-    end
-    return patch (client)
+function Library.client (t)
+  local client = setmetatable ({
+    _results = {},
+    _waiting = {},
+  }, Client)
+  client.host     = t.host     or false
+  client.port     = t.port     or 80
+  client.username = t.username or false
+  client.password = t.password or false
+  local url = "ws://%{host}:%{port}/ws" % {
+    host = client.host,
+    port = client.port,
+  }
+  if _G.js then
+    client._ws = _G.js.new (_G.js.global.WebSocket, url, "cosy")
+  else
+    local Websocket = require "websocket"
+    client._ws = Websocket.client.ev {
+      timeout = Configuration.client.timeout._,
+      loop    = Scheduler._loop,
+    }
   end
+  client._ws.onopen     = function ()
+    Scheduler.wakeup (client._co)
+  end
+  client._ws.onclose    = function ()
+  end
+  client._ws.onmessage  = function (_, event)
+    local message 
+    if _G.js then
+      message = event.data
+    else
+      message = event
+    end
+    message = Value.decode (message)
+    local identifier = message.identifier
+    if identifier then
+      client._results [identifier] = message
+      Scheduler.wakeup (client._waiting [identifier])
+    end
+  end
+  client._ws.onerror    = function ()
+  end
+  if not _G.js then
+    client._ws:on_open (client._ws.onopen)
+    client._ws:on_close (client._ws.onclose)
+    client._ws:on_message (client._ws.onmessage)
+    client._ws:on_error (client._ws.onerror)
+  end
+  if _G.js then
+    client._co = coroutine.running ()
+    Scheduler.sleep (-math.huge)
+  else
+    Scheduler.addthread (function ()
+      client._ws:connect (url, "cosy")
+      client._co = coroutine.running ()
+      Scheduler.sleep (-math.huge)
+    end)
+    Scheduler.loop ()
+  end
+  return patch (client)
+end
 
+if _G.js then
   function Library.connect (url)
     local parser   = _G.js.global.document:createElement "a";
     parser.href    = url;
     local hostname = parser.hostname
     local port     = parser.port
-    local client   = Library.client ("ws://%{hostname}:%{port}/ws" % {
-      hostname = hostname,
+    return Library.client {
+      host     = hostname,
       port     = port,
-    })
-    client.username = parser.username
-    client.password = parser.password
-    return client
-  end
-
-else
-
-  local Scheduler     = require "cosy.scheduler"
-  local Url           = require "socket.url"
-  local Websocket     = require "websocket"
-
-  function Client.react (client)
-    while true do
-      local message = client._ws:receive ()
-      if not message then
-        return
-      end
-      message = Value.decode (message)
-      local identifier = message.identifier
-      if identifier then
-        client._results [identifier] = message
-        Scheduler.wakeup (client._waiting [identifier])
-      else
-        client.on_update ()
-      end
-    end
-  end
-
-  function Client.loop (client)
-    Scheduler.addthread (Client.react, client)
-    Scheduler.loop ()
-  end
-
-  function Client.__index (client, operation)
-    local result = function (parameters, try_only)
-      local result  = nil
-      local coreact = Scheduler.addthread (Client.react, client)
-      Scheduler.addthread (function ()
-        local co = coroutine.running ()
-        local identifier = #client._waiting+1
-        client._waiting [identifier] = co
-        client._results [identifier] = nil
-        client._ws:send (Value.expression {
-          identifier = identifier,
-          operation  = operation,
-          parameters = parameters or {},
-          try_only   = try_only,
-        })
-        Scheduler.sleep (Configuration.client.timeout._)
-        result = client._results [identifier]
-        client._waiting [identifier] = nil
-        client._results [identifier] = nil
-        Scheduler.kill (coreact)
-      end)
-      Scheduler.loop ()
-      if result == nil then
-        error {
-          _ = "client:timeout",
-        }
-      elseif result.success then
-        return result.response
-      else
-        error (result.error)
-      end
-    end
-    return result
-  end
-
-  function Library.client (url)
-    local client = setmetatable ({
-      _waiting = {},
-      _results = {},
-      _react   = nil,
-    }, Client)
-    client._ws = Websocket.client.copas {
-      timeout = Configuration.client.timeout._,
+      username = parser.username,
+      password = parser.password,
     }
-    Scheduler.addthread (function ()
-      client._ws:connect (url, "cosy")
-    end)
-    Scheduler.loop ()
-    return patch (client)
   end
-
+else
+  local Url = require "socket.url"
   function Library.connect (url)
     local parsed   = Url.parse (url)
-    local host     = parsed.host
-    local port     = parsed.port
-    local client   = Library.client ("ws://%{host}:%{port}/ws" % {
-      host = host,
-      port = port,
-    })
-    client.username = parsed.user
-    client.password = parsed.password
-    return client
+    return Library.client {
+      host     = parsed.host,
+      port     = parsed.port,
+      username = parsed.user,
+      password = parsed.password,
+    }
   end
-
 end
 
 return Library
