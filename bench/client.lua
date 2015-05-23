e2830e6506fd8f09da122622d4512b663f791200
Alban Linard <alban@linard.fr>
2015-05-23 23:26:39 +0200

    Small fix of client.

--- bench/client.lua
+++ bench/client.lua
@@ -4,12 +4,6 @@ local I18n          = require "cosy.i18n"
 local Library       = require "cosy.library"
 local Value         = require "cosy.value"
 
-do
-  os.execute "redis-cli flushall"
-end
-
-local token = arg [1]
-
 local ok, err = pcall (function ()
   local function show (name, x)
     print (name, Value.expression (x))
@@ -17,10 +11,11 @@ local ok, err = pcall (function ()
   local lib   = Library.connect "http://127.0.0.1:8080/"
 --  local lib   = Library.connect "http://cosy.linard.fr/"
   local info  = lib.information ()
---  show ("information", info)
+  show ("information", info)
 --  print (lib.statistics ())
   local tos   = lib.tos ()
   show ("terms of service", tos)
+--[[
   local token = lib.create_user {
     username   = "alinard",
     password   = "password",
@@ -34,6 +29,7 @@ local ok, err = pcall (function ()
     password = "password",
   }
   show ("authenticate", token)
+--]]
 --  local result = lib.suspend_user {
 --    token    = token,
 --    username = "alinard",
@@ -66,9 +62,6 @@ local ok, err = pcall (function ()
   local finish = require "socket".gettime ()
   print (math.floor (n / (finish - start)), "requests/second")
   --]==]
-  lib.stop {
-    token = token,
-  }
 end)
 if not ok then
   if type (err) == "table" then
