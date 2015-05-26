4d6e4d07e9d8e95eca20be8da51d22c4652c84de
Alban Linard <alban@linard.fr>
2015-05-26 22:07:08 +0200

    Add pidfiles to configuration.

--- src/cosy/configuration/default.lua
+++ src/cosy/configuration/default.lua
@@ -19,12 +19,14 @@ return {
     port      = 0,
     data_file = os.getenv "HOME" .. "/.cosy/server.data",
     log_file  = os.getenv "HOME" .. "/.cosy/server.log",
+    pid_file  = os.getenv "HOME" .. "/.cosy/server.pid",
   },
   daemon = {
     interface = "127.0.0.1",
     port      = 0,
     data_file = os.getenv "HOME" .. "/.cosy/daemon.data",
     log_file  = os.getenv "HOME" .. "/.cosy/daemon.log",
+    pid_file  = os.getenv "HOME" .. "/.cosy/daemon.pid",
   },
   library = {
     timeout = 2,
