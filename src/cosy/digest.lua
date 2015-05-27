f76d8d11aec7924da33b483d18c1ef3a6cc1c1ef
sofiane <osboxes@osboxes>
2015-05-27 11:54:12 +0200

    Digest.lua: use jquery to eval sjcl script, and use sha256

--- src/cosy/digest.lua
+++ src/cosy/digest.lua
@@ -2,10 +2,11 @@ local Loader = require "cosy.loader"
 
 if _G.js then
   local script = Loader.loadhttp "/js/sjcl.js"
-  _G.js.global:eval (script)
+  --_G.js.global:eval (script)
+  window.jQuery:globalEval (script)
   return function (s)
-    local out = _G.js.global.sjcl.hash.sha512:hash (s)
-    return _G.js.global.codec.hex:fromBits (out)
+    local out = _G.js.global.sjcl.hash.sha256:hash (s)
+    return _G.js.global.sjcl.codec.hex:fromBits (out)
   end
 else
   return function (s)
