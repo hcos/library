36107746e037f5441866111f75a310859be4b4dc
Alban Linard <alban@linard.fr>
2015-05-28 10:02:19 +0200

    Fix locale: replace _ with -.

--- src/cosy/configuration/default.lua
+++ src/cosy/configuration/default.lua
@@ -78,7 +78,7 @@ return {
   statistics = "http://stats.cosyverif.org",
   cli = {
     directory      = os.getenv "HOME" .. "/.cosy",
-    default_locale = (os.getenv "LANG" or "en"):match "[^%.]+",
+    default_locale = (os.getenv "LANG" or "en"):match "[^%.]+":gsub ("_", "-"),
     default_server = "http://cosyverif.org/",
   },
 }
