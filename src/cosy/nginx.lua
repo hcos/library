47db077b47f8da28034e2dd94c176a6d6562f992
Alban Linard <alban@linard.fr>
2015-06-04 21:47:54 +0200

    Remove handling of file upload by nginx.

--- src/cosy/nginx.lua
+++ src/cosy/nginx.lua
@@ -114,32 +114,6 @@ http {
       ';
     }
 
-  location /upload {
-    content_by_lua '
-      local upload = require "resty.upload"
-      local cjson  = require "cjson"
-      local chunk_size = 5 -- 8192
-      local form, err = upload:new (chunk_size)
-      if not form then
-        ngx.log (ngx.ERR, "failed to new upload: ", err)
-        ngx.exit (500)
-      end
-      form:set_timeout(1000) -- 1 second
-      while true do
-        local typ, res, err = form:read ()
-        if not typ then
-          ngx.log (ngx.ERR, "failed to read: ", err)
-          return
-        end
-        ngx.say ("read: ", cjson.encode({typ, res}))
-        if typ == "eof" then
-          break
-        end
-      end
-      local typ, res, err = form:read ()
-      ngx.say ("read: ", cjson.encode({typ, res}))
-    ';
-    }
   }
 }
 ]]
