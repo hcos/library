d2713a17d215f8c9cc021bfd4cde6b433c6ef23a
Sofiane Achouba <achouba@gmail.com>
2015-07-07 11:51:17 +0200

    aplly again the temporary solution for loadhttp in library, the sync mode did not work

--- src/cosy/library.lua
+++ src/cosy/library.lua
@@ -152,23 +152,28 @@ Client.methods ["user:update"] = function (operation, parameters)
   if parameters.password then
     parameters.password = Digest (parameters.password)
   end
-  if parameters.position then
-   local url = "http://maps.googleapis.com/maps/api/geocode/json?address={{{country}}},{{{city}}}" % {
+  if parameters.position and parameters.position.longitude == "" and parameters.position.latitude == "" then
+  local co = coroutine.wrap(function() -- FIXME
+    local url = "http://maps.googleapis.com/maps/api/geocode/json?address={{{country}}},{{{city}}}" % {
       country = parameters.position.country,
      city = parameters.position.city,
     }
     local response, status = Loader.loadhttp (url)
     if status == 200 then
       local coordinate = Json.decode (response)
-      parameters.position = {
-        country   = parameters.position.country,
-        city      = parameters.position.city,
-        latitude = coordinate.results [1].geometry.location.lat,
-        longitude = coordinate.results [1].geometry.location.lng,
-      }
+      position = parameters.position
+      position.latitude  = coordinate.results [1].geometry.location.lat
+      position.longitude = coordinate.results [1].geometry.location.lng
+      client.user.update ({
+      authentication = parameters.authentication,
+      position = position,
+      })
     end
+    end)
+  co ()
+  else
+    local result = mcoroutine.yield ()
   end
-  local result = mcoroutine.yield ()
   if result.success and parameters.username then
     data.username = result.response.username
     data.token    = result.response.authentication
