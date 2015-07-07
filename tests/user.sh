1c9b68645a15b741d6dfbe9908742ce641b82371
Alban Linard <alban@linard.fr>
2015-07-07 14:13:15 +0200

    Update small test script.

--- tests/user.sh
+++ tests/user.sh
@@ -4,12 +4,12 @@ passwords=$(mktemp)
 echo "password" >> ${passwords}
 echo "password" >> ${passwords}
 
-echo "Stopping daemon:"
-./cosy.lua daemon:stop --force
-echo "Stopping server:"
-./cosy.lua server:stop  --force
-echo "Starting server:"
-./cosy.lua server:start --force --clean
+#echo "Stopping daemon:"
+#./cosy.lua daemon:stop --force
+#echo "Stopping server:"
+#./cosy.lua server:stop  --force
+#echo "Starting server:"
+#./cosy.lua server:start --force --clean
 echo "Printing available methods:"
 ./cosy.lua
 echo "Server information:"
@@ -28,13 +28,17 @@ echo "Authenticating user alban:"
 cat ${passwords} | ./cosy.lua user:information alban
 echo "Updating user alban:"
 ./cosy.lua user:update --name="Alban Linard"
+echo "Sending validation again:"
+./cosy.lua user:send-validation
 echo "Showing user alban:"
 ./cosy.lua user:information alban
-echo "Showing the list of users:"
-./cosy.lua user:list
 echo "Deleting user alinard:"
 ./cosy.lua user:delete
 echo "Failing at authenticating user alinard:"
 cat ${passwords} | ./cosy.lua user:authenticate alinard
 echo "Authenticating user alban:"
 cat ${passwords} | ./cosy.lua user:authenticate alban
+echo "Deleting user alban:"
+./cosy.lua user:delete
+
+rm ${passwords}
