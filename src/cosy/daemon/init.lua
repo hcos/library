1083ba6906db0b30dea907f3f98b76bacc101e40
Alban Linard <alban@linard.fr>
2015-08-27 10:33:42 +0200

    Remove handlers.

--- src/cosy/daemon/init.lua
+++ src/cosy/daemon/init.lua
@@ -1,7 +1,7 @@
                       require "cosy.loader"
 local Configuration = require "cosy.configuration"
 local I18n          = require "cosy.i18n"
-local Handler       = require "cosy.daemon.handler"
+local Library       = require "cosy.library"
 local Logger        = require "cosy.logger"
 local Value         = require "cosy.value"
 local Scheduler     = require "cosy.scheduler"
@@ -14,7 +14,10 @@ Configuration.load {
   "cosy.server",
 }
 
-local i18n   = I18n.load "cosy.daemon"
+local i18n   = I18n.load {
+  "cosy.daemon",
+  "cosy.server",
+}
 i18n._locale = Configuration.locale
 
 local Daemon = {}
@@ -49,7 +52,43 @@ function Daemon.start ()
             })
           elseif message then
             Scheduler.addthread (function ()
-              local response = Handler (message)
+              local decoded, request = pcall (Value.decode, message)
+              if not decoded or type (request) ~= "table" then
+                return Value.expression (i18n {
+                  success = false,
+                  error   = i18n {
+                    _ = i18n ["message:invalid"] % {},
+                  },
+                })
+              end
+              local server = request.server
+              local lib    = Daemon.libraries [server]
+              if not lib then
+                lib = Library.connect (server)
+                if not lib then
+                  return Value.expression (i18n {
+                    success = false,
+                    error   = {
+                      _ = i18n ["server:unreachable"] % {},
+                    },
+                  })
+                end
+                Daemon.libraries [server] = lib
+              end
+              local method = lib [request.operation]
+              local result, err = method (request.parameters, request.try_only)
+              if result then
+                result = {
+                  success  = true,
+                  response = result,
+                }
+              else
+                result = {
+                  success = false,
+                  error   = err,
+                }
+              end
+              local response = Value.expression (result)
               Logger.debug {
                 _        = i18n ["daemon:response"],
                 request  = message,
--- src/cosy/server/init.lua
+++ src/cosy/server/init.lua
@@ -6,12 +6,13 @@ local Configuration = require "cosy.configuration"
 local Digest        = require "cosy.digest"
 local I18n          = require "cosy.i18n"
 local Logger        = require "cosy.logger"
+local Methods       = require "cosy.methods"
 local Nginx         = require "cosy.nginx"
 local Random        = require "cosy.random"
 local Redis         = require "cosy.redis"
 local Scheduler     = require "cosy.scheduler"
-local Handler       = require "cosy.server.handler"
 local Store         = require "cosy.store"
+local Value         = require "cosy.value"
 local Token         = require "cosy.token"
 local Value         = require "cosy.value"
 local App           = require "cosy.configuration.layers".app
@@ -19,6 +20,8 @@ local Default       = require "cosy.configuration.layers".default
 local Layer         = require "layeredata"
 local Websocket     = require "websocket"
 local Ffi           = require "ffi"
+
+Configuration.load "cosy.server"
 
 local i18n   = I18n.load "cosy.server"
 i18n._locale = Configuration.locale
@@ -123,6 +126,62 @@ function Server.setname ()
   }
 end
 
+local function deproxify (t)
+  if type (t) ~= "table" then
+    return t
+  else
+    t = Store.export (t)
+    local result = {}
+    for k, v in pairs (t) do
+      assert (type (k) ~= "table")
+      result [k] = deproxify (v)
+    end
+    return result
+  end
+end
+
+function Server.call_method (method, parameters, try_only)
+  for _ = 1, Configuration.redis.retry or 1 do
+    local store  = Store.new ()
+    store = Store.specialize (store, Configuration.server.token)
+    local err
+    local ok, result = xpcall (function ()
+      local r = method (parameters, store, try_only)
+      if not try_only then
+        Store.commit (store)
+      end
+      return r
+    end, function (e)
+      if tostring (e):match "ERR MULTI" then
+        store.__redis:discard ()
+      elseif type (e  ) == "table"
+         and type (e._) == "table"
+         and e._._key   ~= "redis:retry" then
+        err = e
+      else
+        Logger.debug {
+          _      = i18n ["server:exception"],
+          reason = Value.expression (e) .. " => " .. debug.traceback (),
+        }
+      end
+    end)
+    if ok then
+      return deproxify (result) or true
+    elseif err then
+      return nil, err
+    end
+  end
+  return nil, {
+    _ = i18n ["error:internal"],
+  }
+end
+
+function Server.call_parameters (method, parameters)
+  parameters.__DESCRIBE = true
+  local _, result = pcall (method, parameters)
+  return result
+end
+
 function Server.start ()
   if not Configuration.http.hostname then
     Server.sethostname ()
@@ -154,7 +213,65 @@ function Server.start ()
           }
           if message then
             Scheduler.addthread (function ()
-              local response = Handler (message, ws)
+              local result, err
+              local decoded, request = pcall (Value.decode, message)
+              if not decoded or type (request) ~= "table" then
+                result = Value.expression (i18n {
+                  success = false,
+                  error   = {
+                    _ = i18n ["message:invalid"],
+                  },
+                })
+              end
+              if not result then
+                local identifier      = request.identifier
+                local operation       = request.operation
+                local parameters      = request.parameters or {}
+                local try_only        = request.try_only
+                local parameters_only = false
+                local method          = Methods
+                if operation:sub (-1) == "?" then
+                  operation       = operation:sub (1, #operation-1)
+                  parameters_only = true
+                end
+                for name in operation:gmatch "[^:]+" do
+                  if method ~= nil then
+                    name = name:gsub ("-", "_")
+                    method = method [name]
+                  end
+                end
+                if not method then
+                  return Value.expression (i18n {
+                    identifier = identifier,
+                    success    = false,
+                    error      = {
+                      _         = i18n ["server:no-operation"],
+                      operation = request.operation,
+                    },
+                  })
+                end
+                if parameters_only then
+                  result      = Server.call_parameters (method, parameters)
+                else
+                  result, err = Server.call_method (method, parameters, try_only)
+                end
+                if type (result) == "function" then
+                  -- TODO
+                elseif result then
+                  result = i18n {
+                    identifier = identifier,
+                    success    = true,
+                    response   = result,
+                  }
+                else
+                  result = i18n {
+                    identifier = identifier,
+                    success    = false,
+                    error      = err,
+                  }
+                end
+              end
+              local response = Value.expression (result)
               Logger.debug {
                 _        = i18n ["server:response"],
                 request  = message,
