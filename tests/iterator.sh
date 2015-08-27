c7b30faa38679d6c446cd93e3753403efea61fb6
Alban Linard <alban@linard.fr>
2015-08-27 16:12:07 +0200

    Add small examples for iteration on users.

--- tests/iterator.sh
+++ tests/iterator.sh
@@ -2,3 +2,13 @@
 
 ./cosy.lua server:filter "return function (yield, store) for i = 1, 10 do yield (i, i) end end"
 ./cosy.lua server:filter "return function (yield, store) for i = 1, 10 do yield (i, i) end; error 'abcd' end"
+./cosy.lua server:filter 'return function (yield, store)
+    for key, user in store / "data" / ".*" do
+      yield { key = key, name = user.username, }
+    end
+  end'
+./cosy.lua server:filter 'return function (yield, store)
+    for key, user in store / "data" / "a" do
+      yield { key = key, name = user.username, }
+    end
+  end'
