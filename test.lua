d29a8cedab9a5d4666a0daabf8f3fdf37e0e322b
Sofiane Achouba <achouba@gmail.com>
2015-09-16 13:52:53 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,11 +1,22 @@
-for value in _G.client.server.filter({
-        iterator = [[return function (store)
-          for k,user in store / "data" / "[^/]*" do
-            if user.projectname == nil then
-              coroutine.yield {lat = user.position.latitude , lng = user.position.longitude}
-            end
-          end
-        end]]
-      }) do
-        if value.lat and value.lng then
-          iframe.cluster (nil,value.lat,value.lng)
+   local event = coroutine.yield ()
+    if event == "register" then
+      local user  = js.global.document:getElementById("username").value
+      local pass  = js.global.document:getElementById("pass").value
+      local email  = js.global.document:getElementById("email").value
+      local captcha  = js.global.document:getElementById("g-recaptcha-response").value
+      local tostext  = client.server.tos ()
+      local ip = loader.loadhttp "/ip"
+      local result, err = client.user.create {
+      username   = user,
+      password   = pass,
+      email      = email ,
+      captcha    = captcha,
+      ip         = ip,
+      tos_digest = tostext.tos_digest:upper (),
+      locale     = "en",
+      }
+     window:jQuery('.overlay'):hide()
+     if result then
+        window:jQuery('#success #message'):html("User Created")
+        window:jQuery('#success'):show()
+        window:jQuery('#register'):hide()
