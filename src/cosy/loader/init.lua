808cc5fab0138b6488db5deb1c6f6ac5bebb05a4
Alban Linard <alban@linard.fr>
2015-09-28 01:14:27 +0200

    Do not check Lua version.

--- src/cosy/loader/init.lua
+++ src/cosy/loader/init.lua
@@ -1,8 +1,10 @@
+--[[
 if not package.searchpath
 or #setmetatable ({}, { __len = function () return 1 end }) ~= 1
 then
   error "Cosy requires Lua >= 5.2 or Luajit with 5.2 compatibility to run."
 end
+--]]
 
 local Loader = {}
 
