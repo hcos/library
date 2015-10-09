fc13c8e24c060d9a5d0fa05423c9173fe7606f99
Alban Linard <alban@linard.fr>
2015-10-09 12:04:54 +0200

    test/user.sh know takes one parameter: the cosy command.

--- tests/user.sh
+++ tests/user.sh
@@ -1,65 +1,67 @@
 #! /bin/bash
 
+cosy="$1"
+
 passwords=$(mktemp)
-echo "password" >> ${passwords}
-echo "password" >> ${passwords}
+echo "password" >> "${passwords}"
+echo "password" >> "${passwords}"
 
 #echo "Stopping daemon:"
-#./cosy.lua daemon:stop --force
+#"${cosy}" daemon:stop --force
 #echo "Stopping server:"
-#./cosy.lua server:stop  --force
+#"${cosy}" server:stop  --force
 #echo "Starting server:"
-#./cosy.lua server:start --force --clean
+#"${cosy}" server:start --force --clean
 echo "Printing available methods:"
-./cosy.lua
+"${cosy}"
 echo "Server information:"
-./cosy.lua server:information
+"${cosy}" server:information
 echo "Terms of Service:"
-./cosy.lua server:tos
+"${cosy}" server:tos
 echo "Creating user alinard:"
-cat ${passwords} | ./cosy.lua user:create alban.linard@gmail.com alinard
+"${cosy}" user:create "alban.linard@gmail.com" alinard < "${passwords}"
 echo "Failing at creating user alban:"
-cat ${passwords} | ./cosy.lua user:create alban.linard@gmail.com alban
+"${cosy}" user:create "alban.linard@gmail.com" alban < "${passwords}"
 echo "Creating user alban:"
-cat ${passwords} | ./cosy.lua user:create jiahua.xu16@gmail.com alban
+"${cosy}" user:create "jiahua.xu16@gmail.com" alban < "${passwords}"
 echo "Authenticating user alinard:"
-cat ${passwords} | ./cosy.lua user:authenticate alinard
+"${cosy}" user:authenticate alinard < "${passwords}"
 echo "Authenticating user alban:"
-cat ${passwords} | ./cosy.lua user:authenticate alban
+"${cosy}" user:authenticate alban < "${passwords}"
 echo "Updating user alban:"
-./cosy.lua user:update --name="Alban Linard" --email=alban.linard@lsv.ens-cachan.fr
+"${cosy}" user:update --name="Alban Linard" --email="alban.linard@lsv.ens-cachan.fr"
 echo "Sending validation again:"
-./cosy.lua user:send-validation
+"${cosy}" user:send-validation
 echo "Showing user alban:"
-./cosy.lua user:information alban
+"${cosy}" user:information alban
 echo "Deleting user alban:"
-./cosy.lua user:delete
+"${cosy}" user:delete
 echo "Failing at authenticating user alban:"
-cat ${passwords} | ./cosy.lua user:authenticate alban
+"${cosy}" user:authenticate alban < "${passwords}"
 echo "Authenticating user alinard:"
-cat ${passwords} | ./cosy.lua user:authenticate alinard
+"${cosy}" user:authenticate alinard < "${passwords}"
 echo "Creating project"
-./cosy.lua project:create dd
+"${cosy}" project:create dd
 for type in formalism model service execution scenario
 do
   echo "Creating ${type} in project"
-  ./cosy.lua ${type}:create instance-${type} alinard/dd
+  "${cosy}" ${type}:create instance-${type} alinard/dd
 done
 echo "Iterating over users"
-./cosy.lua server:filter 'return function (store)
+"${cosy}" server:filter 'return function (store)
     for user in store / "data" * ".*" do
       coroutine.yield (user)
     end
   end'
 echo "Iterating over projects"
-./cosy.lua server:filter 'return function (store)
+"${cosy}" server:filter 'return function (store)
     for project in store / "data" * ".*" * ".*" do
       coroutine.yield (project)
     end
   end'
 echo "Deleting project"
-./cosy.lua project:delete alinard/dd
+"${cosy}" project:delete alinard/dd
 echo "Deleting user alinard:"
-./cosy.lua user:delete
+"${cosy}" user:delete
 
-rm ${passwords}
+rm "${passwords}"
