ee417bccff19ed9fb5479356d4025d0cf5716ced
Hugues Leprieur <leprieur@lipn.univ-paris13.fr>
2015-10-14 12:48:16 +0200

    Centralises read and write to file. Fix #118

---
+++ src/cosy/file/init.lua
@@ -0,0 +1,21 @@
+local Value = require "cosy.value"
+
+local File = {}
+
+function File.encode (filname, data)
+  local file = io.open (filname, "w")
+  file:write (Value.expression (data))
+  file:close ()
+end
+
+function File.decode (filename)
+  local file, err = io.open (filename, "r")
+  if not file then
+    error (err)
+  end
+  local data = file:read "*all"
+  file:close ()
+  return Value.decode (data)
+end
+
+return File
