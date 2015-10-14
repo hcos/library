3b72bb9757b84f960d8d026ae5d30aca24d5d3e7
Hugues Leprieur <leprieur@lipn.univ-paris13.fr>
2015-10-14 17:41:09 +0200

    Suppress randomize and adapt to decode err

--- src/cosy/file/test.lua
+++ src/cosy/file/test.lua
@@ -5,39 +5,44 @@ require "busted.runner" ()
 local File = require "cosy.file"
 
 describe ("Module cosy.file", function ()
-  randomize (false)  --  tells to play the test in that given order
+-------------------------- commented randomize that is not recognized by luacheck tool
+--------------------------   adapted tests so that they can be passed in any order
+--  randomize (false)  --  tells to play the test in that given order
   --   the first test creates a file and the second test needs that file
 
   local expected_data
   local filename
 
-  setup (function ()
-    filename = os.tmpname ()
-    expected_data = {
-      "First line.",
-    }
- end)
-
-  it ("should save data into a file", function ()
-    File.encode (filename, expected_data)
+  before_each (function ()
+   filename = os.tmpname ()
+   expected_data = {
+     "First line.",
+   }
+  end)
+  after_each (function ()
+    os.remove( filename )
   end)
 
-  it ("should read data from a file", function ()
+  it ("should save then read data into a file", function ()
+    File.encode (filename, expected_data)
     local result_data = File.decode (filename)
     assert.are.same (expected_data, result_data)
   end)
 
   it ("should fail by trying to read a non existing file", function ()
-    os.remove (filename)
-    assert.has.errors (function ()
-      File.decode (filename)
-    end)
+    os.remove( filename )
+    local data, err = File.decode (filename)
+    assert.is_nil (data)
+    print(err)
+    assert.is_not_nil (err)
   end)
 
   it ("should return nil when reading an empty file", function ()
     local file = io.open (filename, "w")
     file:close ()
-    assert.is_nil (File.decode (filename))
+    local data, err = File.decode (filename)
+    assert.is_nil (data)
+    assert.is_nil (err)
   end)
 
 end)
