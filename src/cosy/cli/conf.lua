cf363452e07d4735d30cd02dff5e3a0db4be6427
Hugues Leprieur <leprieur@lipn.univ-paris13.fr>
2015-10-15 16:19:06 +0200

    Add a data file and path to store packages.

--- src/cosy/cli/conf.lua
+++ src/cosy/cli/conf.lua
@@ -5,5 +5,7 @@ Default.cli = {
   directory = os.getenv "HOME" .. "/.cosy",
   locale    = (os.getenv "LANG" or "en"):match "[^%.]+":gsub ("_", "-"),
   log       = os.getenv "HOME" .. "/.cosy/cli.log",
+  packages_directory = os.getenv "HOME" .. "/.cosy/lua",
+  packages_data = os.getenv "HOME" .. "/.cosy/lua.data",
   server    = "http://cosyverif.org/",
 }
