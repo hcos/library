e14a842678f01ec8d5c134d90facf5e5395286a2
Hugues Leprieur <leprieur@lipn.univ-paris13.fr>
2015-10-15 11:26:55 +0200

    Start description of loading algorihm.

--- src/cosy/loader/cli.lua
+++ src/cosy/loader/cli.lua
@@ -1,3 +1,22 @@
+--- When starting Cli (re)-loads the packages from the server
+---      every package has a timestamp & validity duration
+
+-- Cli gets the loader from the server
+-- Cli loader asks the list & eTag of the packages
+-- Cli removes the packages "suppressed by the server"
+-- Cli updates the outdated packages (before updating them)
+-- Cli sends in parallel as many requests as there are packages to download
+-- Cli stores into ~/.cosy/lua the packages downloaded from server
+-- Cli stores into ~/.cosy/lua.data file the table that contains eTag for every package
+--
+------------- 5.1 package.loaders -->  5.2 package.searchers  loader.cli.lua
+-- Cli insert in its searchers at 2n position in its package.searchers table
+--
+---- Configuration
+-- Default.cli = {
+--   packages_directory = os.getenv "HOME" .. "/.cosy/lua",
+--   packages_data = os.getenv "HOME" .. "/.cosy/lua.data",
+-- }
 if #setmetatable ({}, { __len = function () return 1 end }) ~= 1
 then
   error "Cosy requires Lua >= 5.2 or Luajit with 5.2 compatibility to run."
@@ -34,5 +53,14 @@ end)
 
 local Coromake = require "coroutine.make"
 _G.coroutine   = Coromake ()
+
+--[[
+-- tell in which directory should the config be saved
+local directory  = Configuration.cli.directory
+Lfs.mkdir (directory)  -- in the case it does not exist already
+local data_file  = Configuration.cli.data
+-- reads the config
+local saved_config = File.decode (data_file) or {}
+--]]
 
 return Loader
