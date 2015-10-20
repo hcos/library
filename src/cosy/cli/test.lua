fa879fdb161c47c1dbb4a15d6c1c5d976a597586
Alban Linard <alban@linard.fr>
2015-10-20 18:18:11 +0200

    Adapt tests to changes in Cli: use Cli.start and call configure as a method.

--- src/cosy/cli/test.lua
+++ src/cosy/cli/test.lua
@@ -1,23 +1,19 @@
 -- These lines are required to correctly run tests:
 require "cosy.loader.busted"
 require "busted.runner" ()
-local File = require "cosy.file"
+local File          = require "cosy.file"
+local Cli           = require "cosy.cli"
+local Configuration = require "cosy.configuration"
 
 describe ("Module cosy.cli", function ()
 
-  local Cli
-  local Configuration
-
   before_each (function ()
-    package.loaded ["cosy.cli"] = nil  -- reload to reset
-    Cli = require "cosy.cli"
-    Configuration = require "cosy.configuration"
-    Configuration.cli.data = os.tmpname()
+    Configuration.cli.data   = os.tmpname()
     Configuration.cli.server = "dummy_default"  -- override to a known default value
   end)
 
   after_each (function ()
-    os.remove( Configuration.cli.data )
+    os.remove (Configuration.cli.data)
   end)
 
   describe ("parsing options by method configure", function ()
@@ -28,24 +24,27 @@ describe ("Module cosy.cli", function ()
     } do
 
       it ("should detect the --" .. key, function ()
-        Cli.configure {
+        local cli = Cli.new ()
+        cli:configure {
           "--debug=true",
           "--".. key .. "=any_value",
         }
-        assert.are.equal (Cli [key], "any_value")
+        assert.are.equal (cli [key], "any_value")
       end)
 
       it ("should detect --" .. key .. " is missing", function ()
-        Cli.configure {
+        local cli = Cli.new ()
+        cli:configure {
           "--debug=true",
           "-".. key .. "=any_value",
         }
-        assert.are.equal (Cli [key] , "dummy_default")
+        assert.are.equal (cli [key] , "dummy_default")
       end)
 
       it ("should fail by detecting several --" .. key, function ()
+        local cli = Cli.new ()
         assert.has.errors (function ()
-          Cli.configure {
+          cli:configure {
             "--debug=true",
             "--".. key .. "=any_value",
             "--".. key .. "=any_value",
@@ -60,15 +59,16 @@ describe ("Module cosy.cli", function ()
   describe ("saving options by method configure", function ()
 
     it ("should detect the --server", function ()
-      Cli.configure {
+      local cli = Cli.new ()
+      cli:configure {
         "--debug=true",
         "--server=server_uri_from_cmd_line",
       }
       -- assert server was found and set
-      assert.are.equal (Cli.server, "server_uri_from_cmd_line")
+      assert.are.equal (cli.server, "server_uri_from_cmd_line")
       -- assert config was saved to config file
       local saved_config = File.decode (Configuration.cli.data)
-      assert.are.equal (saved_config.server, Cli.server)
+      assert.are.equal (saved_config.server, cli.server)
     end)
 
   end)
