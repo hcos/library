fa4816447ed8ddd479d080b98f67684cccf8c34f
Hugues Leprieur <leprieur@lipn.univ-paris13.fr>
2015-10-23 15:42:34 +0200

    Use hotswaphttp instead of internal downloading.

--- src/cosy/cli/init.lua
+++ src/cosy/cli/init.lua
@@ -3,44 +3,6 @@ for k in pairs (package.loaded) do
   loaded_modules [k] = true
 end
 
-local Loader = require "cosy.loader.cli"
-
-
-local Configuration
-local File
-local I18n
-local Cliargs
-local Colors
-local Lfs
-local Websocket
-local i18n
-
-local function init ()
-
-  Configuration = require "cosy.configuration"
-  File          = require "cosy.file"
-  I18n          = require "cosy.i18n"
-  Cliargs       = require "cliargs"
-  Colors        = require "ansicolors"
-  Lfs           = require "lfs"
-  Websocket     = require "websocket"
-
-  Configuration.load {
-    "cosy.cli",
-    "cosy.daemon",
-  }
-
-  i18n = I18n.load {
-    "cosy.cli",
-    "cosy.commands",
-    "cosy.daemon",
-  }
-  i18n._locale = Configuration.cli.locale
-
-end
-
-init()
-
 
 local Cli = {
   color = true,
@@ -61,71 +23,126 @@ end
 
 function Cli.configure (cli, arguments)
   assert (getmetatable (cli) == Cli)
+
+  local Lfs           = require "lfs"  -- C module : won't be reloaded from server
+  local Json          = require "cjson"  -- lua tables are transcoded into json for server  (pkg comes with lua socket)
+  local  Ltn12         = require "ltn12"  -- to store the content of the requests ( pkgcomes with lua socket)
+
   -- parse  the cmd line arguments to fetch server and/or color options
-  for _, key in ipairs {  -- key to parse
-    "server",
-    "color",
-  } do
-    local pattern = "%-%-" .. key .. "=(.*)"
-    local j = 1
-    while j <= #arguments do
-      local argument = arguments [j]
-      local value = argument:match (pattern)   -- value contains only right hand side of equals
-      if value then -- matched
-        assert (not cli [key])  -- (better)  nil or false
-        cli [key] = value
-        table.remove (arguments, j)
-      else
-        j = j + 1
-      end
+  local key = "server"
+  local pattern = "%-%-" .. key .. "=(.*)"
+  local j = 1
+  while j <= #arguments do
+    local argument = arguments [j]
+    local value = argument:match (pattern)   -- value contains only right hand side of equals
+    if value then -- matched
+      assert (not cli [key])  -- (better)  nil or false
+      cli [key] = value
+      table.remove (arguments, j)
+    else
+      j = j + 1
     end
   end
   cli.arguments = arguments
 
-  -- tell in which directory should the config be saved
-  local directory  = Configuration.cli.directory
-  Lfs.mkdir (directory)  -- in the case it does not exist already
-  local data_file  = Configuration.cli.data
   -- reads the config
-  local saved_config = File.decode (data_file) or {}
-  if cli.server then -- save the server in the config file  ~/.cosy/cli.data
-    saved_config.server = cli.server -- may override the server
-    File.encode (data_file, saved_config)
-  else -- try to fetch the server from the previously saved config
-    if saved_config.server then
-      cli.server = saved_config.server
-    else -- try to fetch the server from the default config
-      if Configuration.cli.server then
-        cli.server = Configuration.cli.server  -- may override the server
-        saved_config.server = cli.server
-        File.encode (data_file, saved_config)
-      end
+  local cosy_dir   = os.getenv "HOME" .. "/.cosy"
+  local data_file  = cosy_dir  .. "/cli-server" -- contains last server uri
+  if not cli.server then ---- try to fetch the server from the previously saved config
+    local file = io.open (data_file,"r")
+    if file then
+        cli.server  = file:read "*all"  -- all the file
+        file:close ()
     end
+  end
+  if not cli.server then ---- still not :  -- try to fetch the server from the default config
+    cli.server = "http://public.cosyverif.lsv.fr"
+    cli.hardcoded_server = true -- warning
   end
   assert (cli.server)
+  -- trim eventuel trailing /   http://server/
+  cli.server  = cli.server:gsub("/+$","")
+  assert (cli.server:match "^https?://")  -- check is an URI
 
-  Loader.server = cli.server
-
-  -- telecharger le nouveau loader
-  local Http = require "socket.http"
-  local body, status = Http.request (cli.server .. "/lua/cosy.loader.cli")
-  if status ~= 200 then -- could not fetch loader
-    return
-  end  -- otherwise keep the same modules
-
-  -- decharger tous les modules already loaded ( directly or by transitivity)
-  for k in pairs (package.loaded) do
-    if not loaded_modules [k] then
-       package.loaded [k] = nil  -- unload every module that was not loaded at startup
+  do -- save server name for next cli launch
+    Lfs.mkdir (cosy_dir)
+    local file, err = io.open (data_file,"w")
+    if file then
+      file:write (cli.server)
+      file:close ()
+    else
+      print (err)
     end
-  end
-  assert (loadstring (body)) ()  -- execute the loader
-  init()   -- reloads locally every modules
-end
+  end -- save server name for next cli launch
+
+
+
+-- hot_swap_http  (  storage_dir , cache)
+  local Mime =  require "mime"
+
+  --  every dowloaded lua package will be saved in ~/.cosy/lua/base64(server_name)
+  local package_dir =  cosy_dir .. "/lua/"
+  local server_dir =  package_dir .. Mime.b64 (cli.server)
+  Lfs.mkdir (package_dir)
+  Lfs.mkdir (server_dir)
+
+  require "copas"  ---- WARNING WE CANNOT WAIT TO GET IT FROM THE SERVER
+
+  local hotswap = require "hotswap.http" {
+    storage = server_dir, -- where to save the lua files
+    encode = function (t)
+      local data = Json.encode (t)
+      return {
+        url     = cli.server .. "/luaset",
+        method  = "POST",
+        headers = {
+          ["Content-Length"] = #data,
+        },
+        source  = Ltn12.source.string (data),
+      }
+    end,
+    decode = function (t)
+      return Json.decode (t.body)
+    end,
+  }
+
+-- In order to download lua modules (required by the client) from the server
+-- we replace the Lua require function
+--      by the hotswap.require which will also save lua packages into "server_dir"
+  _G.require = hotswap.require  --
+  local Loader = require "cosy.loader.cli"  -- "cosy.loader.cli" is downloaded from server
+  Loader.hotswap = hotswap  -- just set the variable (useless ?)
+
+ -- no module to download
+ end
+
 
 function Cli.start (cli)
   assert (getmetatable (cli) == Cli)
+
   cli:configure (_G.arg)
+
+  local Configuration = require "cosy.configuration"
+  local File          = require "cosy.file"
+  local I18n          = require "cosy.i18n"
+  local Cliargs       = require "cliargs"
+  local Colors        = require "ansicolors"
+  local Websocket     = require "websocket"
+
+  Configuration.load {
+    "cosy.cli",
+    "cosy.daemon",
+  }
+
+  local i18n = I18n.load {
+    "cosy.cli",
+    "cosy.commands",
+    "cosy.daemon",
+  }
+  i18n._locale = Configuration.cli.locale
+
+
+
   local daemondata = File.decode (Configuration.daemon.data)
 
   if not cli.color then
--- src/cosy/loader/cli.lua
+++ src/cosy/loader/cli.lua
@@ -1,7 +1,7 @@
 --- When starting Cli (re)-loads the packages from the server
 ---      every package has a timestamp & validity duration
 
--- Cli gets the loader from the server
+-- Cli gets the hotswap loader from the server
 -- Cli loader asks the list & eTag of the packages
 -- Cli removes the packages "suppressed by the server"
 -- Cli updates the outdated packages (before updating them)
@@ -22,84 +22,17 @@ then
   error "Cosy requires Lua >= 5.2 or Luajit with 5.2 compatibility to run."
 end
 
-io.stdout:setvbuf "no"
-
 local Loader = {}
 
 package.preload ["cosy.loader"] = function ()
   return Loader
 end
 
-
 Loader.scheduler = require "copas"
 Loader.nolog     = true
-Loader.hotswap   = require "hotswap" .new {}
 Loader.loadhttp = (require "socket.http").request
 
-
-local Ltn12 = require "ltn12"
-
--- read the file that lists of packages to download
-local Lfs = require "lfs"
-local File = require "cosy.file"
-
-local Configuration = require "cosy.configuration"
-Configuration.load {
-  "cosy.cli",
-}
-local packages_data = File.decode (Configuration.cli.packages_data) or {}
-
-local function http_require (name)
-  -- print ("http_require ",name, Loader.server)
-  if not Loader.server then
-    return nil
-  end
-  local url = Loader.server .. "/lua/" .. name
-  local parts = {}
-  local _, status, headers = Loader.loadhttp {
-    url = url,
-    method = "GET",
-    headers = {
-      ["If-None-Match"] = packages_data [name],
-    },
-    sink = Ltn12.sink.table(parts) -- how to store the result
-  }
-  local result
-  -- eg ~/.cosy/lua/cosy.x.lua for both cosy/x/init.lua   or  cosy/x.lua
-  local local_filename = Configuration.cli.packages_directory .. "/" .. name .. ".lua"
-  -- print ("fetching module ",name, "status ", status, "local_filename", local_filename)
-  if status == 200 then -- OK
-    local content = table.concat (parts)
-    if File.encode (local_filename, content) then
-      packages_data [name] = headers.etag
-    end
-    result = loadstring (content)
-  elseif status == 304 then
-    result = loadfile (local_filename)
-  else
-    packages_data [name] =  nil  -- remove element
-  end
-  File.encode (Configuration.cli.packages_data, packages_data )
-  -- print (" local_filename",local_filename, " result=",result)
-  return result
-end
-
-table.insert (package.searchers, 2, http_require) -- installs package searcher
-_G.require = Loader.hotswap.require
-
-
-
-Lfs.mkdir (Configuration.cli.packages_directory)
-
--- parse the packages file
-for name in pairs (packages_data) do
-  require (name)  -- reload
-end
-
-
-
-
-                 require "cosy.string"
+                  require "cosy.string"
 local Coromake = require "coroutine.make"
 _G.coroutine   = Coromake ()
 
