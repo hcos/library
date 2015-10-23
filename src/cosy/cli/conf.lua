738998d5af17b2d4f66430bf1926a3e4d182bc72
Hugues Leprieur <leprieur@lipn.univ-paris13.fr>
2015-10-23 15:40:34 +0200

    Suppressed data file.

--- src/cosy/cli/conf.lua
+++ src/cosy/cli/conf.lua
@@ -1,7 +1,6 @@
 local Default = require "cosy.configuration.layers".default
 
 Default.cli = {
-  data      = os.getenv "HOME" .. "/.cosy/cli.data",
   directory = os.getenv "HOME" .. "/.cosy",
   locale    = (os.getenv "LANG" or "en"):match "[^%.]+":gsub ("_", "-"),
   log       = os.getenv "HOME" .. "/.cosy/cli.log",
