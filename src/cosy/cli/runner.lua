5dd2f0d8dc5e3ef60a2d78966d20180d9f8dfb65
Alban Linard <alban@linard.fr>
2015-10-28 01:36:10 +0100

    Handle case where server is not defined in cli runner.

--- src/cosy/cli/runner.lua
+++ src/cosy/cli/runner.lua
@@ -3,7 +3,7 @@ local Cli = require "cosy.cli"
 local cli = Cli.new ()
 local ok, err = pcall (cli.start, cli)
 if not ok then
-  print ("An error happened. Maybe the client was unable to download sources from " .. cli.server .. ".")
+  print ("An error happened. Maybe the client was unable to download sources from " .. (cli.server or "no server") .. ".")
   local errorfile = os.tmpname ()
   local file      = io.open (errorfile, "w")
   file:write (tostring (err))
