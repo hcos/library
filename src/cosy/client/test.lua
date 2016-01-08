ad213435d10a70a038111638e519394d897c7bd7
Alban Linard <alban@linard.fr>
2016-01-08 23:37:02 +0100

    Update client test to work with previous changes.

--- src/cosy/client/test.lua
+++ src/cosy/client/test.lua
@@ -4,59 +4,102 @@ _G._TEST = true
 require "busted.runner" ()
 local loader = require "cosy.loader.lua" {
   logto = false,
+  alias = "__busted__", -- server alias
 }
 local Configuration = loader.load "cosy.configuration"
+local File          = loader.load "cosy.file"
 local Cli           = loader.require "cosy.client.cli"
-
-Cli.default_server = "http://127.0.0.1:8080"
-Cli.default_locale = "en"
 
 Configuration.load {
   "cosy.client",
+  "cosy.server",
 }
 Configuration.library = {
   timeout = 1,
 }
 
+local data = assert (File.decode (Configuration.server.data))
+local server_url = "http://127.0.0.1:{{{port}}}" % {
+  port = data.http.port,
+}
+Cli.default_server = server_url
+Cli.default_locale = "en"
+
 describe ("Module cosy.client", function ()
 
   before_each (function ()
-    Configuration.cli.data = os.tmpname()
-  end)
-
-  after_each (function ()
     os.remove (Configuration.cli.data)
   end)
 
   describe ("parsing options by method configure", function ()
 
+    it ("should set the alias if the --alias option is set", function ()
+      local cli = Cli.new ()
+      cli:configure {
+        "--alias=" .. loader.alias,
+      }
+      assert.are.equal (cli.alias, loader.alias)
+    end)
+
+    it ("should set the locale if the --locale option is set", function ()
+      local cli = Cli.new ()
+      cli:configure {
+        "--alias=" .. loader.alias,
+        "--locale=zz",
+      }
+      assert.are.equal (cli.locale, "zz")
+    end)
+
+    it ("should use a default locale if the --locale option is missing", function ()
+      local cli = Cli.new ()
+      cli:configure {
+        "--alias=" .. loader.alias,
+      }
+      assert.are.equal (cli.locale, Cli.default_locale)
+    end)
+
     it ("should set the server if the --server option is set", function ()
       local cli = Cli.new ()
       cli:configure {
-        "--server=http://127.0.0.1:8080",
+        "--alias=" .. loader.alias,
+        "--server=" .. server_url,
       }
-      assert.are.equal (cli.server, "http://127.0.0.1:8080")
+      assert.are.equal (cli.server, server_url)
     end)
 
     it ("should use a default server if the --server option is missing", function ()
       local cli = Cli.new ()
-      cli:configure {}
+      cli:configure {
+        "--alias=" .. loader.alias,
+      }
       assert.are.equal (cli.server, Cli.default_server)
     end)
 
     it ("should pick the last one if several --server options are set", function ()
       local cli = Cli.new ()
       cli:configure {
-        "--server=http://127.0.0.1:8090",
-        "--server=http://127.0.0.1:8080",
+        "--alias=" .. loader.alias,
+        "--server=http://127.0.0.1:0",
+        "--server=" .. server_url,
       }
-      assert.are.equal (cli.server, "http://127.0.0.1:8080")
+      assert.are.equal (cli.server, server_url)
+    end)
+
+    it ("should fail if the --server option is not a cosy server", function ()
+      local cli = Cli.new ()
+      assert.has.errors (function ()
+        cli:configure {
+          "--alias=" .. loader.alias,
+          "--server=http://www.microsoft.com/",
+        }
+      end)
     end)
 
     it ("should fail if the --server option is not a HTTP(s) URL", function ()
       local cli = Cli.new ()
       assert.has.errors (function ()
         cli:configure {
+          "--alias=" .. loader.alias,
           "--server=some.server.org",
         }
       end)
@@ -70,16 +113,19 @@ describe ("Module cosy.client", function ()
       do
         local cli = Cli.new ()
         cli:configure {
-          "--server=http://127.0.0.1:8080",
+          "--alias=" .. loader.alias,
+          "--server=" .. server_url,
         }
         -- assert server was found and set
-        assert.are.equal (cli.server, "http://127.0.0.1:8080")
+        assert.are.equal (cli.server, server_url)
       end
       do
         local cli = Cli.new ()
-        cli:configure {}
+        cli:configure {
+          "--alias=" .. loader.alias,
+        }
         -- assert config was saved to config file
-        assert.are.equal (cli.server, "http://127.0.0.1:8080")
+        assert.are.equal (cli.server, server_url)
       end
     end)
 
