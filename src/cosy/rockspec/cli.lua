f3b1d5920a83a5b97fed80665cac1e63280bc8c0
Alban Linard <alban@linard.fr>
2016-01-09 15:12:21 +0100

    Generate rockspecs for full cosy and client only.

--- src/cosy/rockspec/cli.lua
+++ src/cosy/rockspec/cli.lua
@@ -1,6 +1,12 @@
 local Arguments = require "argparse"
 local Lfs       = require "lfs"
 local Serpent   = require "serpent"
+local Lustache  = require "lustache"
+local Metatable = getmetatable ""
+
+Metatable.__mod = function (pattern, variables)
+  return Lustache:render (pattern, variables)
+end
 
 local parser = Arguments () {
   name        = "cosy-rockspec",
@@ -16,62 +22,102 @@ parser:option "-t" "--target" {
 }
 local arguments = parser:parse ()
 
-local rockspec = {
-  package = "cosyverif",
-  version = "master-1",
-
-  source = {
-    url = "git://github.com/cosyverif/library",
-  },
-
-  description = {
-    summary     = "CosyVerif",
-    detailed    = [[]],
-    homepage    = "http://www.cosyverif.org/",
-    license     = "MIT/X11",
-    maintainer  = "Alban Linard <alban@linard.fr>",
-  },
-
-  dependencies = {
-    "lua >= 5.2",
-    "ansicolors",
-    "argparse",
-    "bcrypt",
-    "copas-ev",
-    "coronest",
-    "dkjson",
-    "hotswap",
-    "hotswap-ev",
-    "hotswap-http",
-    "i18n",
-    "jwt",
-    "layeredata",
-    "lua-cjson-ol", -- needed by jwt, instead of lua-cjson
-    "lua-ev",
-    "lua-resty-http",
-    "lua-websockets",
-    "luacrypto",
-    "luafilesystem",
-    "lualogging",
-    "luaposix",
-    "luasec",
-    "luasocket",
-    "lustache",
-    "md5",
-    "serpent",
-  },
-
-  build = {
-    type    = "builtin",
-    modules = {},
-    install = {
-      bin = {
-        ["cosy"         ] = "src/cosy/client/bin.lua",
-        ["cosy-server"  ] = "src/cosy/server/bin.lua",
-        ["cosy-check"   ] = "src/cosy/check/bin.lua",
-        ["cosy-rockspec"] = "src/cosy/rockspec/bin.lua",
+local rockspecs = {
+  full = {
+    package = "cosyverif",
+    version = "master-1",
+    source = {
+      url = "git://github.com/cosyverif/library",
+    },
+    description = {
+      summary     = "CosyVerif",
+      detailed    = [[]],
+      homepage    = "http://www.cosyverif.org/",
+      license     = "MIT/X11",
+      maintainer  = "Alban Linard <alban@linard.fr>",
+    },
+    dependencies = {
+      "lua >= 5.2",
+      "ansicolors",
+      "argparse",
+      "bcrypt",
+      "copas-ev",
+      "coronest",
+      "dkjson",
+      "hotswap",
+      "hotswap-ev",
+      "hotswap-http",
+      "i18n",
+      "jwt",
+      "layeredata",
+      "lua-cjson-ol", -- needed by jwt, instead of lua-cjson
+      "lua-ev",
+      "lua-resty-http",
+      "lua-websockets",
+      "luacrypto",
+      "luafilesystem",
+      "lualogging",
+      "luaposix",
+      "luasec",
+      "luasocket",
+      "lustache",
+      "md5",
+      "serpent",
+    },
+    build = {
+      type    = "builtin",
+      modules = {},
+      install = {
+        bin = {
+          ["cosy"         ] = "src/cosy/client/bin.lua",
+          ["cosy-server"  ] = "src/cosy/server/bin.lua",
+          ["cosy-check"   ] = "src/cosy/check/bin.lua",
+          ["cosy-rockspec"] = "src/cosy/rockspec/bin.lua",
+        },
+        conf = {},
       },
-      conf = {},
+    },
+  },
+  client = {
+    package = "cosyverif-client",
+    version = "master-1",
+    source = {
+      url = "git://github.com/cosyverif/library",
+    },
+    description = {
+      summary     = "CosyVerif Client",
+      detailed    = [[]],
+      homepage    = "http://www.cosyverif.org/",
+      license     = "MIT/X11",
+      maintainer  = "Alban Linard <alban@linard.fr>",
+    },
+    dependencies = {
+      "lua >= 5.2",
+      "ansicolors",
+      "argparse",
+      "copas",
+      "coronest",
+      "hotswap",
+      "hotswap-http",
+      "i18n",
+      "layeredata",
+      "lua-cjson",
+      "lua-websockets",
+      "luacrypto",
+      "luafilesystem",
+      "lualogging",
+      "luasec",
+      "luasocket",
+      "lustache",
+    },
+    build = {
+      type    = "builtin",
+      modules = {},
+      install = {
+        bin = {
+          ["cosy"         ] = "src/cosy/client/bin.lua",
+        },
+      },
     },
   },
 }
@@ -84,7 +130,7 @@ local function resource (path)
     local subpath = path .. "/" .. content
     if      Lfs.attributes (subpath, "mode") == "file"
     and     not subpath:match "%.lua$"
-    then    rockspec.build.install.conf [#rockspec.build.install.conf+1] = ssource .. "/" .. subpath:sub (#source+2)
+    then    rockspecs.full.build.install.conf [#rockspecs.full.build.install.conf+1] = ssource .. "/" .. subpath:sub (#source+2)
     elseif  content ~= "." and content ~= ".."
     and     Lfs.attributes (subpath, "mode") == "directory"
     then    resource (subpath)
@@ -97,7 +143,7 @@ for module in Lfs.dir (source) do
   if  module ~= "." and module ~= ".."
   and Lfs.attributes (path, "mode") == "directory" then
     if Lfs.attributes (path .. "/init.lua", "mode") == "file" then
-      rockspec.build.modules ["cosy." .. module] = ssource .. "/" .. module .. "/init.lua"
+      rockspecs.full.build.modules ["cosy." .. module] = ssource .. "/" .. module .. "/init.lua"
     end
     for submodule in Lfs.dir (path) do
       if  submodule ~= "." and submodule ~= ".."
@@ -107,12 +153,14 @@ for module in Lfs.dir (source) do
       and not submodule:find "bin%.lua$"
       then
         submodule = submodule:sub (1, #submodule-4)
-        rockspec.build.modules ["cosy." .. module .. "." .. submodule] = ssource .. "/" .. module .. "/" .. submodule .. ".lua"
+        rockspecs.full.build.modules ["cosy." .. module .. "." .. submodule] = ssource .. "/" .. module .. "/" .. submodule .. ".lua"
       end
     end
     resource (path)
   end
 end
+
+rockspecs.client.build.modules = rockspecs.full.build.modules
 
 local options = {
   indent   = "  ",
@@ -125,16 +173,33 @@ local options = {
 }
 
 Lfs.mkdir (arguments.target)
-local file = io.open (arguments.target .. "/cosyverif-master-1.rockspec", "w")
-for _, key in ipairs {
-  "package",
-  "version",
-  "source",
-  "description",
-  "dependencies",
-  "build",
-} do
-  local output = Serpent.block (rockspec [key], options)
-  file:write (key .. " = " .. output .. "\n")
+do
+  local file = io.open (arguments.target .. "/cosyverif-master-1.rockspec", "w")
+  for _, key in ipairs {
+    "package",
+    "version",
+    "source",
+    "description",
+    "dependencies",
+    "build",
+  } do
+    local output = Serpent.block (rockspecs.full [key], options)
+    file:write (key .. " = " .. output .. "\n")
+  end
+  file:close ()
 end
-file:close ()
+do
+  local file = io.open (arguments.target .. "/cosyverif-client-master-1.rockspec", "w")
+  for _, key in ipairs {
+    "package",
+    "version",
+    "source",
+    "description",
+    "dependencies",
+    "build",
+  } do
+    local output = Serpent.block (rockspecs.client [key], options)
+    file:write (key .. " = " .. output .. "\n")
+  end
+  file:close ()
+end
