27de0637be662cecbba6da7fb445d781fff9f513
Alban Linard <alban@linard.fr>
2016-01-20 17:11:46 +0100

    Update www dependencies.

--- src/cosy/nginx/conf.lua
+++ src/cosy/nginx/conf.lua
@@ -26,10 +26,11 @@ return function (loader)
     ["/js/lua.vm.js"] = "https://kripken.github.io/lua.vm.js/lua.vm.js",
     ["/js/sjcl.js"  ] = "http://bitwiseshiftleft.github.io/sjcl/sjcl.js",
     ["/js/jquery.js"] = "http://code.jquery.com/jquery-2.1.4.min.js",
-    ["/js/map.js"] = "https://maps.googleapis.com/maps/api/js?v=3&sensor=false",
+    ["/js/map.js"] = "https://maps.googleapis.com/maps/api/js",
     ["/js/mapcluster.js"] = "http://google-maps-utility-library-v3.googlecode.com/svn/trunk/markerclusterer/src/markerclusterer_compiled.js",
     ["/js/recaptcha.js"] = "https://www.google.com/recaptcha/api.js",
     ["/js/bootstrap.min.js"] = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js",
+    ["/js/position.js"] = "http://rawgit.com/Logicify/jquery-locationpicker-plugin/master/dist/locationpicker.jquery.js",
     ["/css/bootstrap.min.css"] = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css",
     ["/css/font-awesome.min.css"] = "https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css",
     ["/fonts/fontawesome-webfont.woff2"] = "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.3.0/fonts/fontawesome-webfont.woff2",
@@ -38,7 +39,6 @@ return function (loader)
     ["/fonts/glyphicons-halflings-regular.woff2"] = "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/fonts/glyphicons-halflings-regular.woff2",
     ["/fonts/glyphicons-halflings-regular.woff"] = "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/fonts/glyphicons-halflings-regular.woff",
     ["/fonts/glyphicons-halflings-regular.ttf"] = "https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.4/fonts/glyphicons-halflings-regular.ttf",
-    ["/ext/maps" ] = "http://maps.googleapis.com/maps/api/geocode/json",
   }
 
 end
