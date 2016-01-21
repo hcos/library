b614c0d11a16ba2d82b8358c1db0367127656044
Alban Linard <alban@linard.fr>
2016-01-21 17:45:32 +0100

    Remove useless translation.

--- src/cosy/library/i18n.lua
+++ src/cosy/library/i18n.lua
@@ -3,7 +3,6 @@ return function (--[[loader]])
   return {
     ["server:unreachable"] = {
       en = "cosy server is unreachable",
-      fr = "le serveur cosy est injoignable",
     },
     ["server:timeout"] = {
       en = "timeout while waiting response from server",
