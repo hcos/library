dd6a811d4e2f67ecc76cafd9019fe9e83ec0d28b
Alban Linard <alban@linard.fr>
2016-01-22 15:19:40 +0100

    More use of jQuery in authentication.

--- src/cosy/webclient/authentication/init.lua
+++ src/cosy/webclient/authentication/init.lua
@@ -17,13 +17,15 @@ return function (loader)
   Authentication.template.sign_up = Webclient.template "cosy.webclient.authentication.sign-up"
   Authentication.template.log_in  = Webclient.template "cosy.webclient.authentication.log-in"
 
+  Authentication.__index = Authentication
+
   function Authentication.sign_up ()
     local co   = Scheduler.running ()
     local info = Webclient.client.server.information ()
     local tos  = Webclient.client.server.tos {
       locale = Webclient.window.navigator.language,
     }
-    local component = {
+    Webclient.show {
       where    = "main",
       template = Authentication.template.sign_up,
       data     = {
@@ -32,201 +34,200 @@ return function (loader)
       },
       i18n     = i18n,
     }
-    Webclient.show (component)
     local captcha
 
     local function check ()
-      Webclient.window:jQuery "#accept":addClass "disabled"
+      Webclient.jQuery "#accept":addClass "disabled"
       local result, err = Webclient.client.user.create ({
-        identifier = Webclient.document:getElementById "identifier".value,
-        password   = Webclient.document:getElementById "password-1".value,
-        email      = Webclient.document:getElementById "email".value,
-        captcha    = captcha and Webclient.window.grecaptcha:getResponse (captcha),
-        tos_digest = Webclient.document:getElementById "tos".checked
+        identifier = Webclient.jQuery "#identifier":val (),
+        password   = Webclient.jQuery "#password-1":val (),
+        email      = Webclient.jQuery "#email":val (),
+        captcha    = captcha
+                 and Webclient.window.grecaptcha:getResponse (captcha),
+        tos_digest = Webclient.jQuery "#tos":is ":checked"
                  and tos.digest,
-        locale     = Webclient.window.navigator.language,
-      }, true)
+        locale     = Webclient.locale,
+      }, {
+        try_only = true,
+      })
+      local Value = loader.load "cosy.value"
+      print (result, Value.encode (err))
       for _, x in ipairs { "identifier", "email", "password", "captcha", "tos" } do
-        Webclient.window:jQuery ("#" .. x .. "-group"):removeClass "has-error"
-        Webclient.window:jQuery ("#" .. x .. "-group"):addClass    "has-success"
-        Webclient.window:jQuery ("#" .. x .. "-error"):html ("")
+        Webclient.jQuery ("#" .. x .. "-group"):removeClass "has-error"
+        Webclient.jQuery ("#" .. x .. "-group"):addClass    "has-success"
+        Webclient.jQuery ("#" .. x .. "-error"):html ("")
       end
       local passwords = {
-        Webclient.document:getElementById "password-1".value,
-        Webclient.document:getElementById "password-2".value,
+        Webclient.jQuery "#password-1":val (),
+        Webclient.jQuery "#password-2":val (),
       }
       if passwords [1] ~= passwords [2] then
-        Webclient.window:jQuery "#password-group":addClass "has-error"
+        Webclient.jQuery "#password-group":addClass "has-error"
         local text = i18n ["argument:password:nomatch"] % {}
-        Webclient.window:jQuery "#password-error":html (text)
+        Webclient.jQuery "#password-error":html (text)
         result = false
-      else
-        Webclient.window:jQuery "#password-group":addClass "has-success"
-        Webclient.window:jQuery "#password-error":html ("")
       end
       if not captcha or Webclient.window.grecaptcha:getResponse (captcha) == "" then
-        Webclient.window:jQuery "#captcha-group":addClass "has-error"
+        Webclient.jQuery "#captcha-group":addClass "has-error"
         local text = i18n ["sign-up:no-captcha"] % {}
-        Webclient.window:jQuery "#captcha-error":html (text)
+        Webclient.jQuery "#captcha-error":html (text)
         result = false
       end
+      print ("check", result)
       if result then
-        Webclient.window:jQuery "#accept":removeClass "disabled"
+        Webclient.jQuery "#accept":removeClass "disabled"
         return true
       elseif err then
         for _, reason in ipairs (err.reasons or {}) do
           if reason.key == "tos_digest" then
-            Webclient.window:jQuery "#tos-group":addClass "has-error"
+            Webclient.jQuery "#tos-group":addClass "has-error"
             local text = i18n ["sign-up:no-tos"] % {}
-            Webclient.window:jQuery "#tos-error":html (text)
+            Webclient.jQuery "#tos-error":html (text)
           else
-            Webclient.window:jQuery ("#" .. reason.key .. "-group"):addClass "has-error"
-            Webclient.window:jQuery ("#" .. reason.key .. "-error"):html (reason.message)
+            Webclient.jQuery ("#" .. reason.key .. "-group"):addClass "has-error"
+            Webclient.jQuery ("#" .. reason.key .. "-error"):html (reason.message)
           end
         end
         return false
       end
     end
-    for _, x in ipairs { "identifier", "password-1", "password-2", "email", "captcha" } do
-      Webclient.document:getElementById (x).onblur = function ()
-        Webclient.run (check)
-      end
+    for _, x in ipairs { "identifier", "password-1", "password-2", "email", "tos" } do
+      Webclient.jQuery ("#" .. x):focusout (function ()
+        Webclient (check)
+      end)
     end
     for _, x in ipairs { "captcha", "tos" } do
-      Webclient.document:getElementById (x).onchange = function ()
-        Webclient.run (check)
-      end
+      Webclient.jQuery ("#" .. x):change (function ()
+        Webclient (check)
+      end)
     end
-
-    Webclient.document:getElementById "accept".onclick = function ()
+    Webclient.jQuery "#accept":click (function ()
       Scheduler.wakeup (co)
       return false
-    end
+    end)
 
     captcha = Webclient.window.grecaptcha:render ("captcha", Webclient.tojs {
       sitekey  = info.captcha,
       callback = function ()
-        Webclient.run (check)
+        Webclient (check)
       end,
       ["expired-callback"] = function ()
-        Webclient.run (check)
+        Webclient (check)
       end,
     })
 
     while true do
       Scheduler.sleep (-math.huge)
       if check () then
-        Webclient.window:jQuery "#accept":addClass "disabled"
-        Webclient.document:getElementById "accept".innerHTML = [[<i class="fa fa-spinner fa-spin"></i>]]
+        Webclient.jQuery "#accept":addClass "disabled"
+        Webclient.jQuery "#accept":html [[<i class="fa fa-spinner fa-spin"></i>]]
         assert (Webclient.client.user.create {
-          identifier = Webclient.document:getElementById "identifier".value,
-          password   = Webclient.document:getElementById "password-1".value,
-          email      = Webclient.document:getElementById "email".value,
+          identifier = Webclient.jQuery "#identifier":val (),
+          password   = Webclient.jQuery "#password-1":val (),
+          email      = Webclient.jQuery "#email":val (),
           captcha    = captcha
                    and Webclient.window.grecaptcha:getResponse (captcha),
-          tos_digest = Webclient.document:getElementById "tos".checked
+          tos_digest = Webclient.jQuery "#tos":is ":checked"
                    and tos.digest,
-          locale     = Webclient.window.navigator.language,
+          locale     = Webclient.locale,
         })
-        loader.load "cosy.webclient.profile" {
-          where = "main",
-        }
+        loader.load "cosy.webclient.profile" ()
+        return
       end
     end
   end
 
   function Authentication.log_in ()
-    local co        = Scheduler.running ()
-    local component = {
+    local co = Scheduler.running ()
+    Webclient.show {
       where    = "main",
       template = Authentication.template.log_in,
       data     = {},
       i18n     = i18n,
     }
-    Webclient.show (component)
 
-    Webclient.document:getElementById "accept".onclick = function ()
+    Webclient.jQuery "#accept":click (function ()
       Scheduler.wakeup (co)
       return false
-    end
+    end)
 
     while true do
       Scheduler.sleep (-math.huge)
-      Webclient.window:jQuery "#accept":addClass "disabled"
-      Webclient.document:getElementById "accept".innerHTML = [[<i class="fa fa-spinner fa-spin"></i>]]
+      Webclient.jQuery "#accept":addClass "disabled"
+      Webclient.jQuery "#accept":html [[<i class="fa fa-spinner fa-spin"></i>]]
       local result, err = Webclient.client.user.authenticate {
-        user     = Webclient.document:getElementById "identifier".value,
-        password = Webclient.document:getElementById "password"  .value,
-        locale   = Webclient.window.navigator.language,
+        user     = Webclient.jQuery "#identifier":val (),
+        password = Webclient.jQuery "#password"  :val (),
+        locale   = Webclient.locale,
       }
       if result then
-        loader.load "cosy.webclient.dashboard" {
-          where = "main",
-        }
+        loader.load "cosy.webclient.dashboard" ()
         return
       else
-        Webclient.window:jQuery "#accept":removeClass "disabled"
-        Webclient.document:getElementById "accept".innerHTML = [[<i class="fa fa-check"></i>]]
-        Webclient.window:jQuery "#identifier-group":addClass "has-error"
-        Webclient.window:jQuery "#password-group"  :addClass "has-error"
-        Webclient.window:jQuery "#identifier-error":html (err.message)
+        Webclient.jQuery "#accept":removeClass "disabled"
+        Webclient.jQuery "#accept":html [[<i class="fa fa-check"></i>]]
+        Webclient.jQuery "#identifier-group":addClass "has-error"
+        Webclient.jQuery "#password-group"  :addClass "has-error"
+        Webclient.jQuery "#identifier-error":html (err.message)
       end
     end
   end
 
   function Authentication.log_out ()
     Webclient.storage:removeItem "cosy:client"
-    loader.load "cosy.webclient.dashboard" {
-      where = "main",
-    }
+    loader.load "cosy.webclient.dashboard" ()
     Webclient.init ()
   end
 
-  return function (options)
-    Webclient.run (function ()
-      local co        = Scheduler.running ()
-      local component = {
-        where    = options.where,
-        template = Authentication.template.headbar,
-        data     = {
-          user = nil,
-        },
-        i18n     = i18n,
+  local function register_events ()
+    Webclient.jQuery "#sign-up":click (function ()
+      Webclient (function ()
+        Authentication.sign_up ()
+        Scheduler.wakeup (Authentication.co)
+      end)
+      return false
+    end)
+    Webclient.jQuery "#log-in":click (function ()
+      Webclient (function ()
+        Authentication.log_in ()
+        Scheduler.wakeup (Authentication.co)
+      end)
+      return false
+    end)
+    Webclient.jQuery "#log-out":click (function ()
+      Webclient (function ()
+        Authentication.log_out ()
+        Scheduler.wakeup (Authentication.co)
+      end)
+      return false
+    end)
+    Webclient.jQuery "#profile":click (function ()
+      loader.load "cosy.webclient.profile" {
+        where = "main",
       }
+      return false
+    end)
+  end
+
+  function Authentication.__call ()
+    Webclient (function ()
+      Authentication.co = Scheduler.running ()
       while true do
         local user = Webclient.client.user.authentified_as {}
-        component.data.user = user and user.identifier or nil
-        Webclient.show (component)
-        Webclient.document:getElementById "sign-up".onclick = function ()
-          Webclient.run (function ()
-            Authentication.sign_up ()
-            Scheduler.wakeup (co)
-          end)
-          return false
-        end
-        Webclient.document:getElementById "log-in" .onclick = function ()
-          Webclient.run (function ()
-            Authentication.log_in ()
-            Scheduler.wakeup (co)
-          end)
-          return false
-        end
-        Webclient.document:getElementById "log-out".onclick = function ()
-          Webclient.run (function ()
-            Authentication.log_out ()
-            Scheduler.wakeup (co)
-          end)
-          return false
-        end
-        Webclient.document:getElementById "profile" .onclick = function ()
-          loader.load "cosy.webclient.profile" {
-            where = "main",
-          }
-          return false
-        end
+        Webclient.show {
+          where    = "headbar-user",
+          template = Authentication.template.headbar,
+          data     = {
+            user = user and user.identifier or nil,
+          },
+          i18n     = i18n,
+        }
+        register_events ()
         Scheduler.sleep (-math.huge)
       end
     end)
   end
+
+  return setmetatable ({}, Authentication)
 
 end
