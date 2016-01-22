d8d244ee84508a49bbdb2495e955b5fdfca57447
Alban Linard <alban@linard.fr>
2016-01-22 15:18:24 +0100

    More use of jQuery in headbar.

--- src/cosy/webclient/headbar/init.lua
+++ src/cosy/webclient/headbar/init.lua
@@ -7,34 +7,36 @@ return function (loader)
   }
 
   local HeadBar    = {}
+  HeadBar.__index  = HeadBar
   HeadBar.template = Webclient.template "cosy.webclient.headbar"
 
-  return function (options)
-    Webclient.run (function ()
+  local function setmargin ()
+    local height = Webclient.jQuery ".navbar-fixed-top":height ()
+    Webclient.jQuery ".main-content":css (Webclient.tojs {
+      ["margin-top"] = tostring (height + 1) .. "px",
+    })
+  end
+
+  function HeadBar.__call ()
+    Webclient (function ()
       Webclient.show {
-        where    = options.where,
+        where    = "headbar",
         template = HeadBar.template,
         data     = {
           title = Webclient.client.server.information {}.name,
         },
         i18n     = i18n,
       }
-      Webclient.window:eval [[
-        var height = $(".navbar-fixed-top").height ();
-        $(".main-content").css ({
-          "margin-top": (height + 1 ) + "px"
-        });
-      ]]
-      Webclient.document:getElementById "home".onclick = function ()
-        loader.load "cosy.webclient.dashboard" {
-          where = "main",
-        }
+      setmargin ()
+      Webclient.jQuery (Webclient.window):resize (setmargin)
+      Webclient.jQuery "#home":click (function ()
+        loader.load "cosy.webclient.dashboard" ()
         return false
-      end
-      loader.load "cosy.webclient.authentication" {
-        where = "headbar:user",
-      }
+      end)
+      loader.load "cosy.webclient.authentication" ()
     end)
   end
+
+  return setmetatable ({}, HeadBar)
 
 end
