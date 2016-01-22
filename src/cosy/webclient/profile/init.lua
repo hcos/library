6c3da0f491975ab0a3cb3a0adcc559c18cf94e8a
Alban Linard <alban@linard.fr>
2016-01-22 15:18:53 +0100

    More use of jQuery in profile.

--- src/cosy/webclient/profile/init.lua
+++ src/cosy/webclient/profile/init.lua
@@ -14,189 +14,192 @@ return function (loader)
   local Profile = {
     template = {},
   }
+  Profile.__index = Profile
   Profile.template.show = Webclient.template "cosy.webclient.profile.show"
   Profile.template.edit = Webclient.template "cosy.webclient.profile.edit"
 
-  return function (options)
-    Webclient.run (function ()
-      local component = {
-        where    = options.where,
-        template = nil,
-        data     = {},
-        i18n     = i18n,
-      }
-      while true do
-        local user = Webclient.client.user.authentified_as {}
-        options.username = options.username or user.identifier
-        if user and options.username == user.identifier then
+  local function check (info, t)
+    Webclient.jQuery "#accept":addClass "disabled"
+    local email        = Webclient.jQuery "#email":val ()
+    local name         = Webclient.jQuery "#name":val ()
+    local organization = Webclient.jQuery "#organization":val ()
+    local homepage     = Webclient.jQuery "#homepage":val ()
+    local password     = Webclient.jQuery "#password-1":val ()
+    local result, err  = Webclient.client.user.update ({
+      email        = email        ~= (info.email        or "") and email        or nil,
+      avatar       = t.avatar                                  and t.avatar     or nil,
+      name         = name         ~= (info.name         or "") and name         or nil,
+      organization = organization ~= (info.organization or "") and organization or nil,
+      homepage     = homepage     ~= (info.homepage     or "") and homepage     or nil,
+      password     = password     ~= ""                        and password     or nil,
+      position     = t.position                                and t.position   or nil,
+      locale       = Webclient.locale,
+    }, {
+      try_only = true,
+    })
+    for _, x in ipairs { "avatar", "name", "organization", "email", "homepage", "position" } do
+      Webclient.jQuery ("#" .. x .. "-group"):removeClass "has-error"
+      Webclient.jQuery ("#" .. x .. "-error"):html ""
+      if  Webclient.jQuery ("#" .. x):val () ~= info [x] then
+        Webclient.jQuery ("#" .. x .. "-group"):addClass "has-success"
+      end
+    end
+    for _, x in ipairs { "password-1", "password-2" } do
+      Webclient.jQuery ("#" .. x .. "-group"):removeClass "has-error"
+      Webclient.jQuery ("#" .. x .. "-error"):html ""
+      if  Webclient.jQuery ("#" .. x):val () ~= "" then
+        Webclient.jQuery ("#" .. x .. "-group"):addClass "has-success"
+      end
+    end
+    if t.position then
+      Webclient.jQuery "#position-group":addClass "has-success"
+    end
+    local passwords = {
+      Webclient.jQuery "#password-1":val (),
+      Webclient.jQuery "#password-2":val (),
+    }
+    if passwords [1] ~= passwords [2] then
+      Webclient.jQuery "#password-group":addClass "has-error"
+      local text = i18n ["argument:password:nomatch"] % {}
+      Webclient.jQuery "#password-error":html (text)
+      result = false
+    end
+    if result then
+      Webclient.jQuery "#accept":removeClass "disabled"
+      return true
+    elseif err then
+      for _, reason in ipairs (err.reasons or {}) do
+        Webclient.jQuery ("#" .. reason.key .. "-group"):addClass "has-error"
+        Webclient.jQuery ("#" .. reason.key .. "-error"):html (reason.message)
+      end
+      return false
+    end
+  end
 
-          local info = Webclient.client.user.update {}
-          local position
-          local avatar
-
-          local function check ()
-            Webclient.window:jQuery "#accept":addClass "disabled"
-            local email        = Webclient.document:getElementById "email".value
-            local name         = Webclient.document:getElementById "name".value
-            local organization = Webclient.document:getElementById "organization".value
-            local homepage     = Webclient.document:getElementById "homepage".value
-            local password     = Webclient.document:getElementById "password-1".value
-            local result, err  = Webclient.client.user.update ({
-              email        = email ~= info.email and email        or nil,
-              avatar       = avatar              and avatar       or nil,
-              name         = name         ~= ""  and name         or nil,
-              organization = organization ~= ""  and organization or nil,
-              homepage     = homepage     ~= ""  and homepage     or nil,
-              password     = password     ~= ""  and password     or nil,
-              position     = position            and position     or nil,
-              locale       = Webclient.window.navigator.language,
-            }, true)
-            for _, x in ipairs { "avatar", "name", "organization", "email", "homepage", "position", "password-1", "password-2" } do
-              Webclient.window:jQuery ("#" .. x .. "-group"):removeClass "has-error"
-              Webclient.window:jQuery ("#" .. x .. "-error"):html ("")
-              if Webclient.document:getElementById (x).value ~= (info [x] or "") then
-                Webclient.window:jQuery ("#" .. x .. "-group"):addClass "has-success"
-              end
-            end
-            if position then
-              Webclient.window:jQuery ("#position-group"):addClass "has-success"
-            end
-            local passwords = {
-              Webclient.document:getElementById "password-1".value,
-              Webclient.document:getElementById "password-2".value,
+  function Profile.__call (options)
+    Webclient (function ()
+      local co   = Scheduler.running ()
+      local user = Webclient.client.user.authentified_as {}
+      options.username = options.username or user.identifier
+      if user and options.username == user.identifier then
+        local t = {
+          username = options.username or user.identifier,
+          position = nil,
+          avatar   = nil,
+        }
+        local info = Webclient.client.user.update {}
+        Webclient.show {
+          where    = "main",
+          template = Profile.template.edit,
+          data     = info,
+          i18n     = i18n,
+        }
+        if not info.position and Webclient.navigator.geolocation then
+          Webclient.navigator.geolocation:getCurrentPosition (function (_, p)
+            info.position = {
+              latitude  = p.coords.latitude,
+              longitude = p.coords.longitude,
             }
-            if passwords [1] ~= passwords [2] then
-              Webclient.window:jQuery "#password-group":addClass "has-error"
-              local text = i18n ["argument:password:nomatch"] % {}
-              Webclient.window:jQuery "#password-error":html (text)
-              result = false
-            elseif passwords [1] ~= "" then
-              Webclient.window:jQuery "#password-group":addClass "has-success"
-              Webclient.window:jQuery "#password-error":html ("")
-            end
-            if result then
-              Webclient.window:jQuery "#accept":removeClass "disabled"
-              return true
-            elseif err then
-              for _, reason in ipairs (err.reasons or {}) do
-                Webclient.window:jQuery ("#" .. reason.key .. "-group"):addClass "has-error"
-                Webclient.window:jQuery ("#" .. reason.key .. "-error"):html (reason.message)
-              end
-              return false
-            end
-          end
-
-          local co = Scheduler.running ()
-          component.template     = Profile.template.edit
-          component.data         = info
-          component.data.address = info.position and "{{{city}}}, {{{country}}}" % info.position
-          Webclient.show (component)
-
-          if not info.position and Webclient.navigator.geolocation then
-            Webclient.navigator.geolocation:getCurrentPosition (function (_, p)
-              info.position = {
-                latitude  = p.coords.latitude,
-                longitude = p.coords.longitude,
-              }
-              Scheduler.wakeup (co)
-            end)
-            Scheduler.sleep (-math.huge)
-          end
-          Webclient.window:jQuery "#position":locationpicker (Webclient.tojs {
-            location     = info.position,
-            radius       = 0,
-            inputBinding = {
-              locationNameInput = Webclient.window:jQuery "#address",
-            },
-            enableAutocomplete = true,
-            onchanged          = function ()
-              local location = Webclient.window:jQuery "#position":locationpicker "map".location
-              position = {
-                address   = location.formattedAddress,
-                latitude  = location.latitude,
-                longitude = location.longitude,
-              }
-              Webclient.run (check)
-            end,
-            oninitialized      = function ()
-              local location = Webclient.window:jQuery "#position":locationpicker "map".location
-              position = {
-                address   = location.formattedAddress,
-                latitude  = location.latitude,
-                longitude = location.longitude,
-              }
-              Webclient.run (check)
-            end,
-          })
+            Scheduler.wakeup (co)
+          end)
+          Scheduler.sleep (-math.huge)
+        end
+        Webclient.jQuery "#position":locationpicker (Webclient.tojs {
+          location     = info.position,
+          radius       = 0,
+          inputBinding = {
+            locationNameInput = Webclient.jQuery "#address",
+          },
+          enableAutocomplete = true,
+          onchanged          = function ()
+            local location = Webclient.jQuery "#position":locationpicker "map".location
+            t.position = {
+              address   = location.formattedAddress,
+              latitude  = location.latitude,
+              longitude = location.longitude,
+            }
+            Webclient (check, info, t)
+          end,
+          oninitialized      = function ()
+            local location = Webclient.jQuery "#position":locationpicker "map".location
+            t.position = {
+              address   = location.formattedAddress,
+              latitude  = location.latitude,
+              longitude = location.longitude,
+            }
+            Webclient (check, info, t)
+          end,
+        })
+        while true do
           for _, x in ipairs { "name", "organization", "homepage", "email", "password-1", "password-2" } do
-            Webclient.document:getElementById (x).onblur = function ()
-              Webclient.run (check)
-            end
+            Webclient.jQuery ("#" .. x):focusout (function ()
+              Webclient (check, info, t)
+            end)
           end
-          Webclient.document:getElementById "avatar-button".onchange = function ()
+          Webclient.jQuery "#avatar-button":change (function ()
             local reader = Webclient.js.new (Webclient.window.FileReader)
             reader.onload = function ()
-              avatar = reader.result:match "base64,(.*)"
-              Webclient.document:getElementById "avatar".src = "data:image/*;base64," .. avatar
-              Webclient.run (check)
+              Webclient.jQuery "#avatar":attr ("src", reader.result)
+              t.avatar = reader.result:match "base64,(.*)"
+              Webclient (check, info, t)
             end
-            local button = Webclient.document:getElementById "avatar-button"
-            reader:readAsDataURL (button.files [0])
-          end
-          Webclient.document:getElementById "accept".onclick = function ()
+            local button = Webclient.jQuery "#avatar-button"
+            reader:readAsDataURL (button:prop "files" [0])
+          end)
+          Webclient.jQuery "#accept":click (function ()
             Scheduler.wakeup (co)
             return false
-          end
-          Webclient.document:getElementById "delete".onclick = function ()
-            Webclient.window.bootbox:confirm (i18n ["profile:delete"] % {}, function (result)
+          end)
+          Webclient.jQuery "#delete":click (function ()
+            Webclient.jQuery "bootbox":confirm (i18n ["profile:delete"] % {}, function (result)
               if result ~= Webclient.js.null then
-                Webclient.run (function ()
-                  Webclient.document:getElementById "delete".innerHTML = [[<i class="fa fa-spinner fa-spin"></i>]]
+                Webclient (function ()
+                  Webclient.jQuery "#delete":html [[<i class="fa fa-spinner fa-spin"></i>]]
                   assert (Webclient.client.user.delete {})
-                  Webclient.window:jQuery "#log-out":click ()
+                  Webclient.jQuery "#log-out":click ()
                 end)
               end
             end)
             return false
-          end
+          end)
           Scheduler.sleep (-math.huge)
-          local email        = Webclient.document:getElementById "email".value
-          local name         = Webclient.document:getElementById "name".value
-          local organization = Webclient.document:getElementById "organization".value
-          local homepage     = Webclient.document:getElementById "homepage".value
-          local password     = Webclient.document:getElementById "password-1".value
-          Webclient.document:getElementById "accept".innerHTML = [[<i class="fa fa-spinner fa-spin"></i>]]
+          local email        = Webclient.jQuery "#email":val ()
+          local name         = Webclient.jQuery "#name":val ()
+          local organization = Webclient.jQuery "#organization":val ()
+          local homepage     = Webclient.jQuery "#homepage":val ()
+          local password     = Webclient.jQuery "#password-1":val ()
+          Webclient.jQuery "#accept":html [[<i class="fa fa-spinner fa-spin"></i>]]
           assert (Webclient.client.user.update {
-            email        = email ~= info.email and email        or nil,
-            avatar       = avatar              and avatar       or nil,
-            name         = name         ~= ""  and name         or nil,
-            organization = organization ~= ""  and organization or nil,
-            homepage     = homepage     ~= ""  and homepage     or nil,
-            password     = password     ~= ""  and password     or nil,
-            position     = position            and position     or nil,
-            locale       = Webclient.window.navigator.language,
+            email        = email        ~= (info.email        or "") and email        or nil,
+            avatar       = t.avatar                                  and t.avatar     or nil,
+            name         = name         ~= (info.name         or "") and name         or nil,
+            organization = organization ~= (info.organization or "") and organization or nil,
+            homepage     = homepage     ~= (info.homepage     or "") and homepage     or nil,
+            password     = password     ~= ""                        and password     or nil,
+            position     = t.position                                and t.position   or nil,
+            locale       = Webclient.locale,
           })
-          Webclient.document:getElementById "accept".innerHTML = [[<i class="fa fa-check"></i>]]
-
-        else
-
-          local info = Webclient.client.user.information {
-            user = options.username,
-          }
-          component.template      = Profile.template.show
-          component.data          = info
-          component.data.address  = info.position and "{{{city}}}, {{{country}}}" % info.position
-          Webclient.show (component)
-          local params = {
-            location     = info.position,
-            radius       = 0,
-          }
-          Webclient.window:jQuery "#position":locationpicker (Webclient.tojs (params))
-          Scheduler.sleep (-math.huge)
-
+          Webclient.jQuery "#accept":html [[<i class="fa fa-edit"></i>]]
+          Webclient.jQuery "#accept":addClass "disabled"
         end
+      else
+        local info = Webclient.client.user.information {
+          user = options.username,
+        }
+        Webclient.show {
+          where    = "main",
+          template = Profile.template.show,
+          data     = info,
+          i18n     = i18n,
+        }
+        Webclient.jQuery "#position":locationpicker (Webclient.tojs {
+          location     = info.position,
+          radius       = 0,
+        })
       end
     end)
   end
+
+  return setmetatable ({}, Profile)
 
 end
