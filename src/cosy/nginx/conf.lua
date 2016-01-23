6acb309c5b64f4e6263f85be5da0bc11d109e49a
Alban Linard <alban@linard.fr>
2016-01-23 21:57:13 +0100

    Nginx loads www files from luarocks installed resources.

--- src/cosy/nginx/conf.lua
+++ src/cosy/nginx/conf.lua
@@ -3,11 +3,15 @@ return function (loader)
   local Lfs     = loader.require "lfs"
   local Default = loader.load "cosy.configuration.layers".default
 
-  -- Compute www path:
-  local main = loader.hotswap.searchpath ("cosy.nginx", package.path)
-  if main:sub (1, 1) == "." then
-    main = Lfs.currentdir () .. "/" .. main
+  local www = loader.prefix .. "/lib/luarocks/rocks/cosyverif/"
+  for subpath in Lfs.dir (www) do
+    if  subpath ~= "." and subpath ~= ".."
+    and Lfs.attributes (www .. "/" .. subpath, "mode") == "directory" then
+      www = www .. subpath .. "/src/cosy/www"
+      break
+    end
   end
+  assert (www:match "/www$")
 
   Default.http = {
     nginx         = loader.prefix .. "/nginx",
@@ -18,7 +22,7 @@ return function (loader)
     pid           = loader.home .. "/nginx.pid",
     configuration = loader.home .. "/nginx.conf",
     directory     = loader.home .. "/nginx",
-    www           = main:gsub ("cosy/nginx.*", "cosy/www"),
+    www           = www,
     bundle        = loader.source .. "/cosy-full.lua",
   }
 
