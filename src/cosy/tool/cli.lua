7bf0f748394e4ec19e3f6f1cff60f94943dbb348
Alban Linard <alban@linard.fr>
2016-02-09 11:13:45 +0100

    Update Layer.require to automatically add labels. Fix #190

--- src/cosy/tool/cli.lua
+++ src/cosy/tool/cli.lua
@@ -52,9 +52,23 @@ do
 end
 
 do
-  local oldrequire = Layer.require
   Layer.require = function (name)
-    return oldrequire (name:gsub ("/", "."))
+    local package = name:gsub ("/", ".")
+    if Layer.loaded [package] then
+      return Layer.loaded [package]
+    else
+      local layer = Layer.new {
+        name = name,
+        data = {
+          [Layer.key.labels] = {
+            [name] = true,
+          }
+        }
+      }
+      local reference = Layer.reference (name)
+      layer = require (package) (Layer, layer, reference) or layer
+      return layer, reference
+    end
   end
 end
 
