5d8bf0ca92414e33d8b299a960ec64f6f4df5366
Hugues Leprieur <leprieur@lipn.univ-paris13.fr>
2016-02-19 20:53:43 +0100

    added d3js demo integration

--- src/cosy/webclient/editor/init.lua
+++ src/cosy/webclient/editor/init.lua
@@ -17,12 +17,86 @@ return function (loader)
       name = "my model",
       data = {},
     }
+    Webclient.window.console:log ("deb000")
+    local nodes = Webclient.tojs ( {  ---------------  car on ne lutilise quen Lua
+    		{ x = width/3 , y = height/2 },
+    		{ x = 2*width/3 , y = height/2 },
+    	})
+    ----  Webclient.window.console:log (nodes[1].x)
+    local links = Webclient.tojs {
+               { source = 0, target = 1 },
+    }
+    local styles = {
+		     {name = "font-size" , value  = "14px"},
+    }	
+
+    local mytext = d3:select "body"
+                 : append "h1"
+                 : style (  styles[1].name , styles[1].value  ) --------------------  : style("font-size", "14px")
+                 : text ("Hello World! v01 ")
+                 : style ('fill', 'darkOrange')
+    Webclient.window.console:log ("deb001")
     Editor.svg    = d3:select "#editor"
                   : append "svg"
                   : attr ("class", "cosy-editor")
                   : attr ("width" , width )
                   : attr ("height", height)
                   : style ("pointer-events", "all")
+    local force   = d3.layout:force ()  ---  ou bien   d3:layout:force ()
+                  : size (  Webclient.tojs { width , height } )  
+                  : nodes ( nodes )
+                  : links ( links )
+    force : linkDistance (width/2)
+
+    local link    = Editor.svg : selectAll ('.link')
+    Webclient.window.console:log ("deb900")
+    Webclient.window.console:log (link)
+    local link2 =      link : data ( links )
+    Webclient.window.console:log ("deb901")
+          link2 =      link2 : enter ()
+    Webclient.window.console:log ("deb902")
+          link2 =      link2 : append ('line')
+    Webclient.window.console:log ("deb903")
+          link2 =      link2 : attr ('class', 'link');
+    Webclient.window.console:log ("deb904")
+    local node    = Editor.svg : selectAll ('.node')
+    Webclient.window.console:log ("deb910")
+    local node2    = node : data ( nodes )
+    Webclient.window.console:log ("deb911")
+          node2    = node2 : enter ()
+    Webclient.window.console:log ("deb912")
+          node2    = node2 : append ('circle')
+    Webclient.window.console:log ("deb913")
+          node2    = node2 : attr ('class', 'node');
+    Webclient.window.console:log ("deb914")
+
+    Webclient.window.console:log ("deb002")
+
+
+    force : on ('end', function()
+                print ("on end")
+            node2 : attr ('r', width/25)
+                  : attr('cx', function (_, d)  Webclient.window.console:log (d); return d.x end)
+                  : attr('cy', function (_, d)  return d.y end)
+            link2 : attr('x1', function (_, d)  Webclient.window.console:log (d); return d.source.x end)
+                  : attr('y1', function (_, d)  return d.source.y end)
+                  : attr('x2', function (_, d)  return d.target.x end)
+                  : attr('y2', function (_, d)  return d.target.y end)
+               end)
+
+
+
+    Webclient.window.console:log ("deb003")
+    force : start()
+    Webclient.window.console:log ("deb004")
+
+
+
+
+
+
+
+
     Editor.outer  = Editor.svg
                   : append "g"
                   : attr ("class", "cosy-editor-outer")
